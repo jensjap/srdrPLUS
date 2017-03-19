b18cd9820f650659500333acaab2460dcd770371
Jens Jap <jens_jap@brown.edu>
2017-03-19 01:15:43 -0400

    Provide files to overwrite devise default views & controllers.
    - To remove, just remove controllers/users and views/users and
      modify config/routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
   get 'help'  => 'static_pages#help'
   get 'about' => 'static_pages#about'
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
 end
