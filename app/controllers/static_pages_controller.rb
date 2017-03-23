51addf538dc98ad0cbdabf0d0a4e28faff0a68c5
Jens Jap <jens_jap@brown.edu>
2017-03-23 14:53:06 -0400

    Home, help and about should not require authentication.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :help, :about]
 
   def home
   end
