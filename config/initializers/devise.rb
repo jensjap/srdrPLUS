5039a5a769a508636b464a204d3c6307bfb17488
Jens Jap <jens_jap@brown.edu>
2017-03-24 15:07:13 -0400

    Specify which layouts to use for Devise controllers.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,3 +275,12 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
+
+# Controller specific layouts.
+Rails.application.config.to_prepare do
+  Devise::SessionsController.layout "static_pages"
+  Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "static_pages" }
+  Devise::ConfirmationsController.layout "static_pages"
+  Devise::UnlocksController.layout "static_pages"
+  Devise::PasswordsController.layout "static_pages"
+end
