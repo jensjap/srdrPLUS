df128e8640428e183ec7c49a9a1a020dcb8ef522
Jens Jap <jens_jap@brown.edu>
2017-04-03 23:28:00 -0400

    Adds more tests for Organization Profile Flow.

--- test/integration/organization_profile_flow_test.rb
+++ test/integration/organization_profile_flow_test.rb
@@ -4,19 +4,28 @@ class OrganizationProfileFlowText < Capybara::Rails::TestCase
   def setup
     sign_in(@test_superadmin)
     visit edit_profile_path
+    assert page.has_content? 'Organization Information'
   end
 
-  test 'selecting "-- Other (new suggestion) --" should make unhide New Organization Name' do
-    assert page.has_content? 'Organization Information'
-
-    refute_selector(:css, '#suggested-organization-fields', visible: :hidden)
-    select '-- Other (suggest new) --', from: 'profile_organization_id'
-    assert_selector(:css, '#suggested-organization-fields')
-    select 'Brown University', from: 'profile_organization_id'
-    refute_selector(:css, '#suggested-organization-fields', visible: :hidden)
+  test 'selecting "-- Other (new suggestion) --" should unhide New Organization Name' do
+    assert page.find('#suggested-organization-fields')[:class].include? 'hide'
+    within_fieldset('Organization Information') do
+      select '-- Other (suggest new) --'
+    end
   end
 
-  test 'selecting "-- Other (new suggestion) --" but not supplying a new organization name should fail validation' do
-    assert page.has_content? 'Organization Information'
+  test 'submitting all of the same fields should not create version entry' do
+    versions_cnt = PaperTrail::Version.count
+    click_on 'Update Profile'
+    assert_equal versions_cnt, PaperTrail::Version.count
+  end
+
+  test 'selecting a different organization should create a version entry' do
+    versions_cnt = PaperTrail::Version.count
+    within_fieldset('Organization Information') do
+      select 'Brown University'
+    end
+    click_on 'Update Profile'
+    assert_equal versions_cnt + 1, PaperTrail::Version.count
   end
 end
