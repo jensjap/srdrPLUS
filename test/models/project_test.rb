1e2e2e0eea91fac589fa7d24ec1e7fc71d7eb415
Jens Jap <jens_jap@brown.edu>
2017-04-09 10:08:36 -0400

    Update fixtures and add test for Project and Publishing.

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @project = projects(:two)
+    @publishing = publishings(:one)
+    @user = users(:two)
+  end
+
+  test 'project responds to publishings' do
+    assert @project.respond_to?(:publishings)
+  end
+
+  test 'publishings can created for project' do
+    previous_publishings_cnt = @project.publishings.count
+    @project.publishings << Publishing.create(publishable: @project, requested_by: @user)
+    assert @project.publishings, previous_publishings_cnt + 1
+  end
 end
--- test/models/publishing_test.rb
+++ test/models/publishing_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class PublishingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @publishing_approved = publishings(:one)
+    @publishing_requested = publishings(:two)
+  end
+
+  test 'requested publishing request is not approved' do
+    assert @publishing_requested.approved_by.blank?
+    assert @publishing_requested.approved_at.blank?
+    refute @publishing_requested.approved?
+  end
+
+  test 'approved publishing request is approved' do
+    assert @publishing_approved.approved_by.present?
+    assert @publishing_approved.approved_at.present?
+    assert @publishing_approved.approved?
+  end
+
+  test 'publishing can be approved' do
+    refute @publishing_requested.approved?
+    @publishing_requested.approve_now(@user)
+    assert @publishing_requested.approved_by.present?
+    assert @publishing_requested.approved_at.present?
+    assert @publishing_requested.approved?
+    assert_equal @publishing_requested.approved_by, @user
+  end
 end
