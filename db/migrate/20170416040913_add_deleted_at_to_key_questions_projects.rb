c74c48bfc6b6dab35f8a46329110286294da61de
Jens Jap <jens_jap@brown.edu>
2017-04-17 16:53:51 -0400

    Some minor changes:
    * Minor stylistic changes.
    * Temporary placeholder links to nav bar.
    * Adds unique index on key_questions_projects table.

--- db/migrate/20170416040913_add_deleted_at_to_key_questions_projects.rb
+++ db/migrate/20170416040913_add_deleted_at_to_key_questions_projects.rb
@@ -2,6 +2,6 @@ class AddDeletedAtToKeyQuestionsProjects < ActiveRecord::Migration[5.0]
   def change
     add_column :key_questions_projects, :deleted_at, :datetime
     add_index :key_questions_projects, :deleted_at
-    add_index :key_questions_projects, [:key_question_id, :project_id, :position], where: 'deleted_at IS NULL'
+    add_index :key_questions_projects, [:key_question_id, :project_id, :position], name: 'index_kqp_on_kq_id_p_id_position', where: 'deleted_at IS NULL'
   end
 end
