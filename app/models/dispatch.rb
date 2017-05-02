ea467829d99e663ab2949bfd0102d78a73584102
Jens Jap <jens_jap@brown.edu>
2017-05-02 13:47:48 -0400

    Fixes Dispatch fixtures.

--- app/models/dispatch.rb
+++ app/models/dispatch.rb
@@ -1,7 +1,7 @@
 class Dispatch < ApplicationRecord
   include SharedParanoiaMethods
 
-  acts_as_paranoid
+  acts_as_paranoid column: :active, sentinel_value: true
   has_paper_trail
 
   belongs_to :dispatchable, polymorphic: true
