233d04d01f27151b6174942673c030134f1e6594
Jens Jap <jens_jap@brown.edu>
2017-06-12 14:53:23 -0400

    Simplified previous code.

--- app/models/concerns/shared_queryable_methods.rb
+++ app/models/concerns/shared_queryable_methods.rb
@@ -13,25 +13,14 @@ module SharedQueryableMethods
     #           it returns an array with a single object element. This will assist in creating
     #           a new Resource later.
     def by_query(query)
-      # Approximate matches first.
-      @resources = where('name like ?', "%#{ query }%")
-      if @resources.blank?
-        # If no approximate matches were found we can return an empty container for creation.
-        return [ OpenStruct.new(id: "<<<#{ query }>>>", name: "New: '#{ query }'") ]
+      # Try exact match first.
+      exact_match = where('name=?', query)
+      return exact_match unless exact_match.blank?
 
-      else
-        # If empty query was sent then we can return the full set of @resources found immediately.
-        return @resources if query.blank?
-
-        # Else, since approximate matches are found we check for exact matches.
-        # We do this in case the user wants to create a resource that consists of a substring
-        # of an existing resource.
-        @exact_match = where('name=?', query)
-        return @exact_match.blank? ?
-          # !Warning: Using '+' actually converts the ActiveRecord to an array which means you can't use
-          # AR methods afterwards.
-          @resources + [ OpenStruct.new(id: "<<<#{ query }>>>", name: "New: '#{ query }'") ] : @resources
-      end
+      # Try approximate matches using 'like'
+      approximate_matches = where('name like ?', "%#{ query }%")
+      return query.blank? ?
+        approximate_matches : approximate_matches + [ OpenStruct.new(id: "<<<#{ query }>>>", name: "New: '#{ query }'") ]
     end
 
     # Params:
