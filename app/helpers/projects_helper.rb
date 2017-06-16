24a1d3046dba91dbfd29b3b920f107703b313140
Jens Jap <jens_jap@brown.edu>
2017-06-16 16:00:03 -0400

    Makes kq assignments more visible.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  def list_of_key_questions(extraction_forms_project)
+    html = '<ul>'
+    extraction_forms_project.key_questions.each do |kq|
+      html += "<li>#{ kq.name }</li>"
+    end
+    html += '</ul>'
+
+    return raw(html)
+  end
 end
