896a7d81bca1d409a5b97937738182a878ea4d26
Jens Jap <jens_jap@brown.edu>
2017-06-20 12:49:34 -0400

    Adds test.

--- test/models/key_questions_project_test.rb
+++ test/models/key_questions_project_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class KeyQuestionsProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @key_questions_project_one   = key_questions_projects(:one)
+    @key_questions_project_two   = key_questions_projects(:one)
+    @extraction_forms_projects_section_type_one   = extraction_forms_projects_sections(:one)
+    @extraction_forms_projects_section_type_two   = extraction_forms_projects_sections(:two)
+    @extraction_forms_projects_section_type_three = extraction_forms_projects_sections(:three)
+    @extraction_forms_projects_section_type_four  = extraction_forms_projects_sections(:four)
+  end
+
+  test 'KeyQuestionsProject should only associate with ExtractionFormsProjectsSectionType of type 4 (Key Questions)' do
+    assert @key_questions_project_one.valid?
+    @key_questions_project_one.extraction_forms_projects_section = @extraction_forms_projects_section_type_one
+    assert_not @key_questions_project_one.valid?
+  end
 end
