fdb6634d127d221eda66506a674ab9f5fcb364c4
Jens Jap <jens_jap@brown.edu>
2017-06-21 11:18:41 -0400

    Keeps Extraction Forms table same height as Key Questions table.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,13 @@
 module ProjectsHelper
   def list_of_key_questions(extraction_forms_project)
-    html = '<ul>'
-    extraction_forms_project.key_questions_projects.each do |kqp|
-      html += "<li>#{ kqp.key_question.name }</li>"
-    end
-    html += '</ul>'
+    if extraction_forms_project.key_questions_projects.present?
+      html = '<ul>'
+      extraction_forms_project.key_questions_projects.each do |kqp|
+        html += "<li>#{ kqp.key_question.name }</li>"
+      end
+      html += '</ul>'
 
-    return raw(html)
+      return raw(html)
+    end
   end
 end
