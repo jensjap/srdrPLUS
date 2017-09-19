00a891973ade170a0555587f5962d6beace3bfb7
Jens Jap <jens_jap@brown.edu>
2017-09-19 15:14:51 -0400

    Just syntax fixes.

--- app/models/extraction_forms_projects_section.rb
+++ app/models/extraction_forms_projects_section.rb
@@ -10,22 +10,25 @@ class ExtractionFormsProjectsSection < ApplicationRecord
 
   belongs_to :extraction_forms_project, inverse_of: :extraction_forms_projects_sections
   belongs_to :extraction_forms_projects_section_type, inverse_of: :extraction_forms_projects_sections
-  belongs_to :section, inverse_of: :extraction_forms_projects_sections
   belongs_to :extraction_forms_projects_section_type1, class_name: 'ExtractionFormsProjectsSection',
                                                        foreign_key: 'extraction_forms_projects_section_id',
                                                        optional: true
+  belongs_to :section, inverse_of: :extraction_forms_projects_sections
 
   has_one :ordering, as: :orderable, dependent: :destroy
 
   has_many :extraction_forms_projects_section_type2s, class_name:  'ExtractionFormsProjectsSection',
-                                                       foreign_key: 'extraction_forms_projects_section_id'
+                                                      foreign_key: 'extraction_forms_projects_section_id'
   has_many :key_questions_projects, dependent: :nullify, inverse_of: :extraction_forms_projects_section
   has_many :key_questions, through: :key_questions_projects, dependent: :destroy
   has_many :questions, dependent: :destroy, inverse_of: :extraction_forms_projects_section
   has_many :type1s, dependent: :destroy, inverse_of: :extraction_forms_projects_section
 
+  delegate :project, to: :extraction_forms_project
+
   validates :ordering, presence: true
 
+  validate :child_type
   validate :parent_type
 
   def section_id=(token)
@@ -37,7 +40,14 @@ class ExtractionFormsProjectsSection < ApplicationRecord
     self.section.name
   end
 
+  #!!! this isn't working.
+  def child_type
+#    errors[:base] << ('Child Type must be of Type 2') unless self.extraction_forms_projects_section_type1.present? &&
+#      self.extraction_forms_projects_section_type_id != 2
+  end
+
   def parent_type
-    errors[:base] << ('Parent Type must be of Type 1') unless self.extraction_forms_projects_section_type1.nil? || self.extraction_forms_projects_section_type1.extraction_forms_projects_section_type_id == 1
+    errors[:base] << ('Parent Type must be of Type 1') unless self.extraction_forms_projects_section_type1.nil? ||
+      self.extraction_forms_projects_section_type1.extraction_forms_projects_section_type_id == 1
   end
 end
