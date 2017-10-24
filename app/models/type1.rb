286e26d186e59c522b06a6f03165976cd68bea72
Jens Jap <jens_jap@brown.edu>
2017-10-24 23:14:10 +0800

    Update type1 model.

--- app/models/type1.rb
+++ app/models/type1.rb
@@ -1,4 +1,14 @@
 class Type1 < ApplicationRecord
+  include SharedQueryableMethods
+  include SharedSuggestableMethods
+
+  acts_as_paranoid
+  has_paper_trail
+
+  after_create :record_suggestor
+
+  has_one :suggestion, as: :suggestable, dependent: :destroy
+
   belongs_to :extraction_forms_projects_section, inverse_of: :type1s
 
   delegate :project, to: :extraction_forms_projects_section
