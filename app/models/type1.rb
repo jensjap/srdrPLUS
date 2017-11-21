bfe1ff073017cd4b6ce6eb4b35569d425cbb118f
Jens Jap <jens_jap@brown.edu>
2017-11-21 11:22:22 +0800

    To find the Type1 in an Extraction we now have a scope in Type1 model.

--- app/models/type1.rb
+++ app/models/type1.rb
@@ -5,6 +5,13 @@ class Type1 < ApplicationRecord
   acts_as_paranoid
   has_paper_trail
 
+  scope :by_section_name_and_extraction_id_and_extraction_forms_project_id, -> (section_name, extraction_id, extraction_forms_project_id) {
+    joins(extractions_extraction_forms_projects_sections_type1s: { extractions_extraction_forms_projects_section: [:extraction, { extraction_forms_projects_section: [:extraction_forms_project, :section] }] })
+      .where(sections: { name: section_name })
+      .where(extractions: { id: extraction_id })
+      .where(extraction_forms_projects: { id: extraction_forms_project_id })
+  }
+
   after_create :record_suggestor
 
   has_one :suggestion, as: :suggestable, dependent: :destroy
@@ -12,7 +19,7 @@ class Type1 < ApplicationRecord
   belongs_to :extraction_forms_projects_section, inverse_of: :type1s
 
   has_many :extractions_extraction_forms_projects_sections_type1s, dependent: :destroy, inverse_of: :type1
-  has_many :extractions_extraction_forms_projects_section, through: :extractions_extraction_forms_projects_sections_type1s, dependent: :destroy
+  has_many :extractions_extraction_forms_projects_sections, through: :extractions_extraction_forms_projects_sections_type1s, dependent: :destroy
 
   delegate :project, to: :extraction_forms_projects_section
 
