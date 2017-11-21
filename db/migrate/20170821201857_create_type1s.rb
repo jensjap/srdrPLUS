f7379c1abdb79eb665ba470a7a178c3a15d707df
Jens Jap <jens_jap@brown.edu>
2017-11-21 11:24:34 +0800

    Make sure name and description are unique for Type1 at db level.

--- db/migrate/20170821201857_create_type1s.rb
+++ db/migrate/20170821201857_create_type1s.rb
@@ -9,6 +9,7 @@ class CreateType1s < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
+    add_index :type1s, [:name, :description], unique: true
     add_index :type1s, :deleted_at
     add_index :type1s, [:extraction_forms_projects_section_id, :deleted_at], name: 'index_t1_on_efps_id_deleted_at', where: 'deleted_at IS NULL'
   end
