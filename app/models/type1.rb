963e0dfb158cff8fe76249e26cd7c64298c3faaf
Jens Jap <jens_jap@brown.edu>
2017-11-26 21:19:30 +0800

    Moves logic into model to find unique Type1s to suggest.

--- app/models/type1.rb
+++ app/models/type1.rb
@@ -5,18 +5,18 @@ class Type1 < ApplicationRecord
   acts_as_paranoid
   has_paper_trail
 
-#  scope :by_section_name_and_extraction_id_and_extraction_forms_project_id, -> (section_name, extraction_id, extraction_forms_project_id) {
-#    joins(extractions_extraction_forms_projects_sections_type1s: { extractions_extraction_forms_projects_section: [:extraction, { extraction_forms_projects_section: [:extraction_forms_project, :section] }] })
-#      .where(sections: { name: section_name })
-#      .where(extractions: { id: extraction_id })
-#      .where(extraction_forms_projects: { id: extraction_forms_project_id })
-#  }
+  scope :uniq_by_section_name_and_not_included, -> (efps_id) {
+    efps = ExtractionFormsProjectsSection.find(efps_id)
+    joins(extraction_forms_projects_sections_type1s: { extraction_forms_projects_section: :section })
+      .where(sections: { name: efps.section.name })
+      .where.not(extraction_forms_projects_sections_type1s: { type1: efps.type1s })
+      .distinct
+  }
 
   after_create :record_suggestor
 
   has_one :suggestion, as: :suggestable, dependent: :destroy
 
-  #belongs_to :extraction_forms_projects_section, inverse_of: :type1s
   has_many :extraction_forms_projects_sections_type1s, dependent: :destroy, inverse_of: :type1
   has_many :extraction_forms_projects_sections, through: :extraction_forms_projects_sections_type1s, dependent: :destroy
 
