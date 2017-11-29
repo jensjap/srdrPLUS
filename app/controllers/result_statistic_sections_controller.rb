f8708673ae4fcbfc333f38619d100e2ff1b8fef3
Jens Jap <jens_jap@brown.edu>
2017-11-29 12:10:03 +0800

    Loads arms into ResultStatisticSection::edit with proper eager loading.

--- app/controllers/result_statistic_sections_controller.rb
+++ app/controllers/result_statistic_sections_controller.rb
@@ -23,7 +23,10 @@ class ResultStatisticSectionsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_result_statistic_section
-      @result_statistic_section = ResultStatisticSection.find(params[:id])
+      @result_statistic_section = ResultStatisticSection
+        .includes(subgroup: { extractions_extraction_forms_projects_sections_type1_row: { extractions_extraction_forms_projects_sections_type1: [:type1, extractions_extraction_forms_projects_section: [:extraction, extraction_forms_projects_section: :extraction_forms_project]] } })
+        .includes(:result_statistic_section_type)
+        .find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
