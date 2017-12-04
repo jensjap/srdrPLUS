969cca1b52d6312e8ff228005844e1d5d99606e5
Jens Jap <jens_jap@brown.edu>
2017-12-04 18:45:54 +0800

    Eager loading.

--- app/controllers/extractions_controller.rb
+++ app/controllers/extractions_controller.rb
@@ -69,7 +69,8 @@ class ExtractionsController < ApplicationController
 
   # GET /extractions/1/work
   def work
-    @extraction_forms_projects = ExtractionFormsProject.by_extraction(@extraction.id)
+    @extraction_forms_projects = ExtractionFormsProject.includes(:extraction_forms_projects_sections)
+                                                       .by_extraction(@extraction.id)
   end
 
   private
