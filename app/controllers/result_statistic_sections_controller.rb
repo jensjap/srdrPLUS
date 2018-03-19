e711b7301f8a21fc1261d730951ac63eb4283157
Birol Senturk <birolsenturk@Birols-MBP.fios-router.home>
2018-03-19 08:17:02 -0400

    great insight

--- app/controllers/result_statistic_sections_controller.rb
+++ app/controllers/result_statistic_sections_controller.rb
@@ -15,6 +15,7 @@ class ResultStatisticSectionsController < ApplicationController
                       notice: t('success') }
         format.json { render :show, status: :ok, location: @result_statistic_section }
       else
+        byebug
         format.html { render :edit }
         format.json { render json: @result_statistic_section.errors, status: :unprocessable_entity }
       end
@@ -41,8 +42,8 @@ class ResultStatisticSectionsController < ApplicationController
     def result_statistic_section_params
       params.require(:result_statistic_section).permit( 
         comparisons_attributes: [ :id, :_destroy, :result_statistic_section_id,
-          comparisons_measures_attributes: [ :id, :_destroy, :comparison_id, :measure_id ],
-          measurements_attributes: [ :id, :_destroy, :comparisons_measure_id, :value ],
+          comparisons_measures_attributes: [ :id, :_destroy, :comparison_id, :measure_id ,
+          measurement_attributes: [ :id, :_destroy, :comparisons_measure_id, :value ] ],
         comparate_groups_attributes: [ :id, :_destroy, :comparison_id, 
         comparates_attributes: [ :id, :_destroy, :comparate_group_id, :comparable_element_id, 
         comparable_element_attributes: [ :id, :_destroy, :comparable_type, :comparable_id, :_destroy ] ] ] ] )
