7f9c81989d9e4b85ed1e5fdbdaf99399cbb34ef9
Birol Senturk <birol_senturk@brown.edu>
2018-04-30 09:05:00 -0400

    initial api commit, nothing works yet

---
+++ app/controllers/api/v1/assignments_controller.rb
@@ -0,0 +1,56 @@
+module Api
+  module V1
+    class ProjectsController < BaseController
+      before_action :set_project, only: [:show, :update, :destroy]
+
+      SORT = {  'updated-at': { updated_at: :desc },
+                'created-at': { created_at: :desc }
+      }.stringify_keys
+
+      def_param_group :project do
+        param :project, Hash, required: true, action_aware: true do
+          param :name,           String, 'Name of the project'
+          param :funding_source, String, 'Who funds this project'
+          param :prospero,       String, 'Prospero ID'
+          param :doi,            String, 'Document Object Identifier'
+          param :description,    String, 'Long description of project'
+          param :attribution,    String, 'Project attribution'
+          param :methodology,    String, 'Methodology description'
+          param :notes,          String, 'Project notes'
+        end
+      end
+
+      api :GET, '/v1/projects', 'List of projects'
+      formats [:json]
+      def index
+        @projects = current_user.projects
+          .includes(:extraction_forms)
+          .includes(:key_questions)
+          .includes(publishings: [{ user: :profile }, approval: [{ user: :profile }]])
+          .by_query(@query).order(SORT[@order]).page(params[:page])
+      end
+
+      api :GET, '/v1/projects/:id', 'Show project by id'
+      param_group :project
+      formats [:json]
+      def show
+      end
+
+      api :POST, '/v1/projects', 'Create project'
+      param_group :project
+      formats ['json']
+      def create
+        @project = Project.new(project_params)
+        flash[:notice] = 'Project was successfully created.' if @project.save
+        respond_with @project
+      end
+
+    end
+
+    private
+
+      def set_project
+        @project = Project.find(params[:id])
+      end
+  end
+end
---
+++ app/views/api/v1/assignments/screen.json.jbuilder
@@ -0,0 +1,23 @@
+json.batch_size @citations_projects.count
+json.citations_projects do
+  json.array!(@citations_projects) do |citations_project|
+    citation = citations_project.citation
+    json.id citations_project.id
+    json.name citation.name
+    json.abstract citation.abstract
+    json.pmid citation.pmid
+    json.refman citation.refman
+    json.journal do
+      json.publication_date citation.journal.publication_date.year
+      json.name citation.journal.name
+      json.volume citation.journal.volume
+      json.issue citation.journal.issue
+    end
+    json.authors do
+      json.array! citation.authors, :id, :name
+    end
+    json.keywords do
+      json.array! citation.keywords, :id, :name
+    end
+  end
+end
