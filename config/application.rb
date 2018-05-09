e04d4e063bb10fab1f0e9a2244520109d69bcddd
Jens Jap <jens_jap@brown.edu>
2018-05-09 08:38:22 +0800

    Don't use namespaces.

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module SrdrPLUS
 
     # Use a real queuing backend for Active Job (and separate queues per environment)
     config.active_job.queue_adapter     = :sidekiq
-    config.active_job.queue_name_prefix = "srdrPLUS_#{Rails.env}"
+    #config.active_job.queue_name_prefix = "srdrPLUS_#{ Rails.env }"
   end
 end
