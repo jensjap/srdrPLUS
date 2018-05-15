bab0d95c48d5352c699a09f950aca303c6aaf764
Jens Jap <jens_jap@brown.edu>
2018-05-15 17:03:10 +0800

    Fixes user sign up.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    user_name:      Rails.application.secrets.mail_username,
-    password:       Rails.application.secrets.mail_password,
-    domain:         'gmail.com',
+#    user_name:      Rails.application.secrets.mail_username,
+#    password:       Rails.application.secrets.mail_password,
+    user_name:     "#{ ENV['MAIL_USERNAME'] }",
+    password:      "#{ ENV['MAIL_PASSWORD'] }",
+    domain:        'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
