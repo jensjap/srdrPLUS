8bd4c5c4a1a8c8a457c3b3648af32102c7f5ddcc
Jens Jap <jens_jap@brown.edu>
2018-05-29 17:43:03 +0800

    Fix renaming in the routes path. No more subgroups...use populations

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
   end
 
   resources :extractions_extraction_forms_projects_sections_type1s, only: [] do
-    get 'get_results_subgroups', on: :member
+    get 'get_results_populations', on: :member
   end
   post '/projects/:id/undo', to: 'projects#undo', as: :undo
   resources :projects, concerns: :paginatable, shallow: true do
