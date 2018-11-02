d4a039bc08044ffec4ef01bd35e63eca71ebe7d9
Randy Jap <randyjap@gmail.com>
2018-11-02 15:00:00 +0800

    skip authorization and scope checks for basic front pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_action :skip_authorization, :skip_policy_scope, only: [:home, :help, :about]
   skip_before_action :authenticate_user!, only: [:home, :help, :about]
 
   SORT = {  'updated-at': { updated_at: :desc },
