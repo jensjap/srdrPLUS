818bb56fcf07c9d1eeed29c70ad716a2f77f55b4
Randy Jap <randyjap@gmail.com>
2018-11-16 15:22:47 +0800

    create and apply assignments controller policy

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,6 +2,9 @@ class AssignmentsController < ApplicationController
   before_action :set_assignment, only: [:screen]
 
   def screen
+    skip_policy_scope
+    authorize(@assignment)
+
     @citations_projects = CitationsProject.unlabeled( @assignment.project, params[:count] )
     @past_labels = Label.last_updated( @assignment.projects_users_role, 0, params[:count] )
     render 'screen'
