39a189158e2d9658a7394e1a2c302bb95368837c
Birol Senturk <birol_senturk@brown.edu>
2018-11-17 22:47:45 -0500

    taggings and notes and reasons work and update as expected

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -25,6 +25,7 @@ class LabelsController < ApplicationController
   end
 
   def update
+    byebug
     respond_to do |format|
       if @label.update( label_params )
         format.json { render json: label_hash( @label ) }
