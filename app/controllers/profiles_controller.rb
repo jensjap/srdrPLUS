beb40b925a0fadc8f62921e0fd62a0557600d347
Randy Jap <randyjap@gmail.com>
2018-11-19 16:52:17 +0800

    refactor and skip scoping

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,20 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, :skip_policy_scope, only: [:show, :edit, :update, :destroy]
+  before_action :call_authorize
+  before_action :skip_policy_scope
 
   # GET /profile
   # GET /profile.json
   def show
-    authorize @profile
   end
 
   # GET /profile/edit
   def edit
-    authorize @profile
   end
 
   # PATCH/PUT /profile
   # PATCH/PUT /profile.json
   def update
-    authorize @profile
     respond_to do |format|
       if @profile.update(profile_params)
         format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
@@ -28,6 +27,10 @@ class ProfilesController < ApplicationController
   end
 
   private
+
+  def call_authorize
+    authorize @profile
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_profile
