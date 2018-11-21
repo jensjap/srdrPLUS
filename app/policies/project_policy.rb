401802bebceffb8c378cf8cd421c660b0dec47ce
Randy Jap <randyjap@gmail.com>
2018-11-21 16:11:25 +0800

    correct project policies

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,7 +1,7 @@
 require_dependency 'app/policies/modules/role_checker'
 
 class ProjectPolicy < ApplicationPolicy
-  extend RoleChecker
+  include RoleChecker
 
   class Scope < ApplicationPolicy::Scope
     def resolve
@@ -9,74 +9,39 @@ class ProjectPolicy < ApplicationPolicy
     end
   end
 
-  def index?
-    ProjectsUser.where(user: user).where(project: record).exists?
-  end
-
-  def show?
-    ProjectsUser.where(user: user).where(project: record).exists?
-  end
-
   def edit?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
+    project_leader?
   end
 
   def update?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
+    project_leader?
   end
 
   def destroy?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
-  end
-
-  def filter?
-    user.present?
+    project_leader?
   end
 
   def undo?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
-  end
-
-  def export?
-    user.present?
+    project_leader?
   end
 
   def import_csv?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
+    project_leader?
   end
 
   def import_pubmed?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
+    project_leader?
   end
 
   def import_ris?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
+    project_leader?
   end
 
   def import_endnote?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
+    project_leader?
   end
 
   def next_assignment?
-    ProjectPolicy.leader_by_user_and_project?(user, record)
-  end
-
-  def comparison_tool?
-    at_least?(RoleChecker::CONSOLIDATOR)
-  end
-
-  def labeled?
-    at_least?(RoleChecker::CONSOLIDATOR)
-  end
-
-  def unlabled?
-    at_least?(RoleChecker::CONSOLIDATOR)
-  end
-
-  private
-
-  def at_least?(role)
-    highest_role = ExtractionPolicy.find_highest_role_id(user, record)
-    highest_role && highest_role <= role
+    project_contributor?
   end
 end
