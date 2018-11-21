f119d60009012d28b1a8473c434c143bff4a2483
Randy Jap <randyjap@gmail.com>
2018-11-21 23:00:23 +0800

    create and apply policies to reasons taggings tags timepoints_names type1s

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,7 +1,11 @@
 module Api
   module V1
     class TagsController < BaseController
+<<<<<<< HEAD
       before_action :set_assignment, only: [ :index ]
+=======
+      before_action :set_projects_user, :skip_policy_scope, :skip_authorization, only: [ :index ]
+>>>>>>> create and apply policies to reasons taggings tags timepoints_names type1s
 
       api :GET, '/v1/projects_users/:id/tags', 'List of tags a user has created'
       formats [:json]
@@ -18,15 +22,53 @@ module Api
           user_tags           = Tag.by_user( @assignment.user ).by_query( query )
           lead_tags           = Tag.by_project_lead( @assignment.project ).by_query( query )
 
+<<<<<<< HEAD
           ## order is important, we want to show projects_users_tags first, then user_tags, then lead_tags
           all_tags = ( lead_tags + projects_user_tags + user_tags ).uniq
+=======
+        paginated_tags = combined_tags[ offset .. offset + page_size - 1 ]
+        @more = if offset + paginated_tags.length < combined_tags.length then true else false end
+        @projects_user_tags = []
+        @user_tags = []
+        @lead_tags = []
+>>>>>>> create and apply policies to reasons taggings tags timepoints_names type1s
 
         end
 
+<<<<<<< HEAD
         offset      = [ page_size * ( page - 1 ), all_tags.length ].min
         @tags       = all_tags[ offset .. offset + page_size - 1 ]
         @more       = if offset + @tags.length < all_tags.length then true else false end
       end
+=======
+#
+#
+#        start_index = offset
+#        end_index = offset + page_size - 1
+#
+#        @projects_user_tags = []
+#        if start_index < projects_user_tags.length
+#          @projects_user_tags = projects_user_tags[ start_index ... [ projects_user_tags.length, end_index ].min ]
+#        end
+#        end_index = [ 0, ( end_index - projects_user_tags.length ) ].max
+#        start_index = [ 0, ( start_index - projects_user_tags.length ) ].max
+#
+#        if start_index <  user_tags.length
+#          @user_tags = user_tags[ start_index ... [ user_tags.length, end_index ].min ]
+#        end
+#        end_index = [ 0, ( end_index - user_tags.length ) ].max
+#        start_index = [ 0, ( start_index - user_tags.length ) ].max
+#
+#        @lead_tags = []
+#        if start_index <  lead_tags.length
+#          @lead_tags = lead_tags[ start_index ... [ lead_tags.length, end_index ].min ]
+#        end
+#        end_index = [ 0, ( end_index - lead_tags.length ) ].max
+#        start_index = [ 0, ( start_index - lead_tags.length ) ].max
+#
+#        @more = end_index > 0
+#        render 'index.json'
+>>>>>>> create and apply policies to reasons taggings tags timepoints_names type1s
 
       private
       def set_assignment
--- app/controllers/api/v1/timepoint_names_controller.rb
+++ app/controllers/api/v1/timepoint_names_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TimepointNamesController < Api::V1::BaseController
+  before_action :skip_policy_scope, :skip_authorization
+
   def index
     @page  = params[:page]
     @query = params[:q]
