3264748712624b9a51e19c560a40b605589545d3
Randy Jap <randyjap@gmail.com>
2018-11-21 17:35:34 +0800

    update project policies

--- app/controllers/api/v1/extractions_controller.rb
+++ app/controllers/api/v1/extractions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ExtractionsController < Api::V1::BaseController
-  before_action :set_project, only: [:index]
+  before_action :set_project, :skip_policy_scope, :skip_authorization, only: [:index]
 
   def index
     #respond_with Extraction.all
