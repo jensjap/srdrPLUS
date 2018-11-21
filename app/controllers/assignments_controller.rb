9062fd38dee2f6339840fe077964ea73db467e7c
Randy Jap <randyjap@gmail.com>
2018-11-21 20:05:27 +0800

    remove unecessary policy

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,10 +1,7 @@
 class AssignmentsController < ApplicationController
-  before_action :set_assignment, only: [:screen]
+  before_action :set_assignment, :skip_policy_scope, :skip_authorization, only: [:screen]
 
   def screen
-    skip_policy_scope
-    authorize(@assignment)
-
     @citations_projects = CitationsProject.unlabeled( @assignment.project, params[:count] )
     @past_labels = Label.last_updated( @assignment.projects_users_role, 0, params[:count] )
     render 'screen'
