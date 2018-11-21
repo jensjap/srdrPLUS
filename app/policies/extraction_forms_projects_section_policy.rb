f05b664b0fc771501b3f3587c738bc551b543a35
Randy Jap <randyjap@gmail.com>
2018-11-21 14:56:04 +0800

    refactor

--- app/policies/extraction_forms_projects_section_policy.rb
+++ app/policies/extraction_forms_projects_section_policy.rb
@@ -1,44 +1,37 @@
 require_dependency 'app/policies/modules/role_checker'
 
 class ExtractionFormsProjectsSectionPolicy < ApplicationPolicy
-  extend RoleChecker
+  include RoleChecker
 
   def new?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def edit?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def create?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def update?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def destroy?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def preview?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def dissociate_type1?
-    at_least?(RoleChecker::CONSOLIDATOR)
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 
   def add_quality_dimension?
-    at_least?(RoleChecker::CONSOLIDATOR)
-  end
-
-  private
-
-  def at_least?(role)
-    highest_role = ExtractionFormsProjectsSectionPolicy.find_highest_role_id(user, record)
-    highest_role && highest_role <= role
+    at_least_project_role?(RoleChecker::CONSOLIDATOR)
   end
 end
