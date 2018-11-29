12ae58ee0f022508ae3d515708273a0cac07ebc6
Randy Jap <randyjap@gmail.com>
2018-11-29 14:44:55 +0800

    fix authorize call

--- app/controllers/key_questions_projects_controller.rb
+++ app/controllers/key_questions_projects_controller.rb
@@ -11,7 +11,7 @@ class KeyQuestionsProjectsController < ApplicationController
   # POST /projects/1/key_questions_projects.json
   def create
     @key_questions_project = @project.key_questions_projects.build(key_questions_project_params)
-    authorize(@key_questions_project)
+    authorize(@project, policy_class: KeyQuestionsProjectPolicy)
 
     respond_to do |format|
       if @key_questions_project.save
