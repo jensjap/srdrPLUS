6965e6cda59869bf7ef8f67b04fb5b6dc62401a0
Jens Jap <jens_jap@brown.edu>
2018-11-30 18:54:32 +0800

    Makes projects navigation available in screening.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,7 +2,8 @@ class AssignmentsController < ApplicationController
   before_action :set_assignment, :skip_policy_scope, :skip_authorization, only: [:screen]
 
   def screen
-    @citations_projects = CitationsProject.unlabeled( @assignment.project, params[:count] )
+    @project = @assignment.project
+    @citations_projects = CitationsProject.unlabeled( @project, params[:count] )
     @past_labels = Label.last_updated( @assignment.projects_users_role, 0, params[:count] )
     render 'screen'
   end
