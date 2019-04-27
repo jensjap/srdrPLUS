35fabb9a15a3c9aa7598983821840fe008ba9a6c
Jens Jap <jens_jap@brown.edu>
2019-04-27 17:41:19 +0800

    Fixes race condition on orderable.

--- app/models/concerns/shared_orderable_methods.rb
+++ app/models/concerns/shared_orderable_methods.rb
@@ -12,7 +12,7 @@ module SharedOrderableMethods
     # Determines and adds ordering to the object within the object's scope.
     def set_ordering_scoped_by(object_scope)
       position = self.class.where("#{ object_scope }": self.send(object_scope)).count + 1
-      create_ordering(position: position) unless self.ordering.present?
+      build_ordering(position: position) unless self.ordering.present?
     end
 
     def position=(new_position)
