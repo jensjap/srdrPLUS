f047cc1483a58e2b6c1ea42372fcd14be08be675
Randy Jap <randyjap@gmail.com>
2019-05-23 12:18:21 +0800

    add results type items to query builder

--- app/controllers/project_report_links_controller.rb
+++ app/controllers/project_report_links_controller.rb
@@ -4,7 +4,8 @@ class ProjectReportLinksController < ApplicationController
   end
 
   def new_query_form
-    project = SdMetaDatum.find(params[:project_report_link_id]).project
+    meta_datum_id = params[:project_report_link_id]
+    project = SdMetaDatum.find(meta_datum_id).project
     if project
       @groups = project.
         questions.
@@ -12,9 +13,35 @@ class ProjectReportLinksController < ApplicationController
         where(key_questions_projects: { id: new_query_params[:kqp_ids] }).
         group_by { |question| question.extraction_forms_projects_section.section }
     end
+
+    @results_groups = get_rssm_groups(project.id)
   end
 
   private
+
+    def get_rssm_groups(project_id)
+      rssms = ResultStatisticSectionsMeasure.
+        joins(result_statistic_section: {
+          population: {
+            extractions_extraction_forms_projects_sections_type1: {
+              extractions_extraction_forms_projects_section: {
+                extraction: :project
+              }
+            }
+          }
+        }).
+        where(result_statistic_section: {
+          population: {
+            extractions_extraction_forms_projects_sections_type1: {
+              extractions_extraction_forms_projects_section: {
+                extractions: { project_id: project_id }
+              }
+            }
+          }
+        }).
+        uniq(&:measure_id)
+      rssms.group_by { |rssm| rssm.result_statistic_section.result_statistic_section_type }
+    end
 
     def new_query_params
       params[:sd_meta_datum] ?
