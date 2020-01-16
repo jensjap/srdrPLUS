656f5a66d6e1ba703a15d24eb17ead7a1b22cbac
Jens Jap <jens_jap@brown.edu>
2020-01-16 18:23:39 +0800

    Ensure KeyQuestionType default options returns a unique list.

--- app/controllers/key_question_types_controller.rb
+++ app/controllers/key_question_types_controller.rb
@@ -10,7 +10,7 @@ class KeyQuestionTypesController < ApplicationController
     if params[:q]
       @key_question_types = KeyQuestionType.by_query(params[:q])
     else
-      @key_question_types = KeyQuestionType.where(name: DEFAULT_KEY_QUESTION_TYPES)
+      @key_question_types = KeyQuestionType.where(name: DEFAULT_KEY_QUESTION_TYPES).uniq
     end
   end
 end
