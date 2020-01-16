b45880ce467eb7fd5db020a0c85720b9ce55a898
Jens Jap <jens_jap@brown.edu>
2020-01-16 10:48:21 +0800

    Key Question Type should default to 4 values.

--- app/controllers/key_question_types_controller.rb
+++ app/controllers/key_question_types_controller.rb
@@ -1,5 +1,16 @@
 class KeyQuestionTypesController < ApplicationController
+  DEFAULT_KEY_QUESTION_TYPES = [
+    "Intervention benefits and/or harms",
+    "Epidemiology - Exposure-outcome relationship",
+    "Epidemiology - Incidence/prevalence",
+    "Diagnostic accuracy"
+  ].freeze
+
   def index
-    @key_question_types = KeyQuestionType.by_query(params[:q])
+    if params[:q]
+      @key_question_types = KeyQuestionType.by_query(params[:q])
+    else
+      @key_question_types = KeyQuestionType.where(name: DEFAULT_KEY_QUESTION_TYPES)
+    end
   end
 end
