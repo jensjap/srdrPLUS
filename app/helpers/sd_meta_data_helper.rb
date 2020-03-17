b802593dca14d3c117cfa98c2f42ca3831c76d58
birol <birol@adminisatorsMBP.fios-router.home>
2020-03-17 17:22:51 -0400

    SRDR 2.0 fixes after group testing

--- app/helpers/sd_meta_data_helper.rb
+++ app/helpers/sd_meta_data_helper.rb
@@ -1,2 +1,13 @@
 module SdMetaDataHelper
+  def sd_url_for(str, name=nil)
+    if str.present?
+      if str.is_url?
+        link_to(name || str, str, target: :_blank)
+      else 
+        str
+      end
+    else
+      "N/A"
+    end
+  end
 end
