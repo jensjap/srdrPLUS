b55669fac2e1b4f34d98f0818f6ccf2d7247ea17
Jens Jap <jens_jap@brown.edu>
2020-04-16 02:39:37 +0800

    updates schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_204904) do
+ActiveRecord::Schema.define(version: 2020_04_10_163130) do
 
   create_table "abstrackr_settings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "profile_id"
