e1e3075c357a163f146cdd0d43135e8ed86b33bc
birol <birol@adminisatorsMBP.fios-router.home>
2020-05-11 18:40:18 -0400

    made rssm orderable

--- app/models/result_statistic_sections_measure.rb
+++ app/models/result_statistic_sections_measure.rb
@@ -12,12 +12,15 @@
 #
 
 class ResultStatisticSectionsMeasure < ApplicationRecord
+  include SharedOrderableMethods
   include SharedParanoiaMethods
 
   acts_as_paranoid column: :active, sentinel_value: true
   has_paper_trail
 
   after_commit :set_extraction_stale, on: [:create, :update, :destroy]
+
+  before_validation -> { set_ordering_scoped_by(:result_statistic_section_id) }
 
   belongs_to :measure,                  inverse_of: :result_statistic_sections_measures
   belongs_to :result_statistic_section, inverse_of: :result_statistic_sections_measures
@@ -33,6 +36,8 @@ class ResultStatisticSectionsMeasure < ApplicationRecord
   has_many :tps_arms_rssms, dependent: :destroy, inverse_of: :result_statistic_sections_measure
   has_many :tps_comparisons_rssms, dependent: :destroy, inverse_of: :result_statistic_sections_measure
   has_many :comparisons_arms_rssms, dependent: :destroy, inverse_of: :result_statistic_sections_measure
+
+  has_one :ordering, as: :orderable, dependent: :destroy
 
   accepts_nested_attributes_for :measure
 
