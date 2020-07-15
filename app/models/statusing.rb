a550fc926009ace3ff575fb9dd6228308a1a1d4a
Birol Senturk <birol_senturk@brown.edu>
2020-07-14 20:51:25 -0400

    made status switch into a switch

--- app/models/statusing.rb
+++ app/models/statusing.rb
@@ -13,11 +13,6 @@
 #
 
 class Statusing < ApplicationRecord
-  include SharedParanoiaMethods
-
-  acts_as_paranoid column: :active, sentinel_value: true
-  has_paper_trail
-
   belongs_to :statusable, polymorphic: true
   belongs_to :status, inverse_of: :statusings
 
