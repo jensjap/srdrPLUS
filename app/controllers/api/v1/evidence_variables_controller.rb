752d05109c4ba335606363ca17ef7fe20645a0c0
Jens Jap <jens_jap@brown.edu>
2020-08-24 22:41:35 -0700

    Fixes a bug with evidence variable api end point.

--- app/controllers/api/v1/evidence_variables_controller.rb
+++ app/controllers/api/v1/evidence_variables_controller.rb
@@ -6,7 +6,11 @@ module Api
       api :GET, '/v1/evidence_variables.json'
       formats [:json]
       def index
-        @evidence_variables = ExtractionsExtractionFormsProjectsSectionsType1.all
+        @evidence_variables = ExtractionsExtractionFormsProjectsSectionsType1
+          .includes(:type1, extractions_extraction_forms_projects_sections_type1_rows:
+                    [extractions_extraction_forms_projects_sections_type1_row_columns: :timepoint_name
+          ])
+          .all
         render 'index.json'
       end
 
@@ -14,8 +18,11 @@ module Api
       formats [:json]
       def show
         @populations = @evidence_variable.extractions_extraction_forms_projects_sections_type1_rows
-        @timepoint_name = @populations.last.extractions_extraction_forms_projects_sections_type1_row_columns.last.timepoint_name.name
-        @timepoint_unit = @populations.last.extractions_extraction_forms_projects_sections_type1_row_columns.last.timepoint_name.unit
+        pop = @populations.try(:last)
+        tps = pop.try(:extractions_extraction_forms_projects_sections_type1_row_columns) if pop
+        tp = tps.try(:last) if tps
+        @timepoint_name = tp.present? ? tp.timepoint_name.name : ''
+        @timepoint_unit = tp.present? ? tp.timepoint_name.unit : ''
       end
 
       private
