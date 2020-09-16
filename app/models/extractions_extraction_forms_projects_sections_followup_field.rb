eddb1394b63dd7c8a625ae55fe08afdaf49eb788
bisen <birol_senturk@brown.edu>
2020-09-16 13:08:07 -0400

    Followup model fix (#172)
    * schema change after migration
    
    * added eefpst1 association to eefps_ff
    
    * created missing eefpsff migration, changed publishable methods to support has_one associations
    
    * added eefps_ff association for eefpst1
    
    Co-authored-by: Jens Jap <jens_jap@brown.edu>

--- app/models/extractions_extraction_forms_projects_sections_followup_field.rb
+++ app/models/extractions_extraction_forms_projects_sections_followup_field.rb
@@ -1,6 +1,7 @@
 class ExtractionsExtractionFormsProjectsSectionsFollowupField < ApplicationRecord
   acts_as_paranoid
-  belongs_to :extractions_extraction_forms_projects_section, inverse_of: :extractions_extraction_forms_projects_sections_followup_fields
+  belongs_to :extractions_extraction_forms_projects_section, inverse_of: :extractions_extraction_forms_projects_sections_followup_fields 
+  belongs_to :extractions_extraction_forms_projects_sections_type1, inverse_of: :extractions_extraction_forms_projects_sections_followup_fields, optional: true 
   belongs_to :followup_field, inverse_of: :extractions_extraction_forms_projects_sections_followup_fields
 
   has_many :records, as: :recordable
