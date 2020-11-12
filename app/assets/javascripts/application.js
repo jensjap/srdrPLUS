ac4ddc5e68c7b3fbfbcd59b1014c46277c82166e
Randy Jap <randyjap@users.noreply.github.com>
2020-11-12 14:32:57 +0800

    Allow orderable items to be selected (#227)
    * add sort handle to key questions
    
    * add more sort handles
    
    Co-authored-by: Jens Jap <jens_jap@brown.edu>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -200,7 +200,6 @@ document.addEventListener( 'turbolinks:load', function() {
     for (let orderable_list of Array.from( $( scope ).find( '.orderable-list' ))) {
       //# CHANGE THIS
       const ajax_url = $( '.orderable-list' ).attr( 'orderable-url' );
-      const forceRestart = $( '.orderable-list' ).attr( 'force-reload' );
       let saved_state = null;
 
       //# helper method for converting class name into camel case
@@ -240,12 +239,7 @@ document.addEventListener( 'turbolinks:load', function() {
               }
               // then save state
               saved_state = $( orderable_list ).sortable( "toArray" );
-              if (forceRestart) {
-                toastr.success( 'Positions successfully updated. Reloading page to apply changes.' );
-                location.reload();
-              } else {
-                toastr.success( 'Positions successfully updated' );
-              }
+              toastr.success( 'Positions successfully updated' );
             },
           error( data ) {
               $( orderable_list ).sortable( 'sort', saved_state );
@@ -261,7 +255,12 @@ document.addEventListener( 'turbolinks:load', function() {
       //# save state when dragging starts
       const on_start =  e  => saved_state = $( orderable_list ).sortable( 'toArray' );
 
-      $( orderable_list ).sortable({ onUpdate: on_update, onStart: on_start });
+      $(orderable_list).sortable({ onUpdate: on_update, onStart: on_start });
+
+      if ($('.sort-handle').length > 0) {
+        $(orderable_list).sortable( "option", "handle", ".sort-handle" );
+      }
+
       saved_state = $( orderable_list ).sortable( 'toArray' );
     }
   }
