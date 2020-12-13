408e075d36909ee60f77802f428e3586e98efb14
Jens Jap <jens_jap@brown.edu>
2020-12-13 01:49:30 -0800

    Documents return value for evidence_variables:{index, show}

--- app/controllers/api/v2/evidence_variables_controller.rb
+++ app/controllers/api/v2/evidence_variables_controller.rb
@@ -8,6 +8,16 @@ class Api::V2::EvidenceVariablesController < Api::V2::BaseController
 
   api :GET, '/v2/evidence_variables.json', 'Full list of Outcome definitions. Requires API Key.'
   param_group :paginate, Api::V2::BaseController
+  returns desc: 'Index of Outcome Definitions; paginate (optional).' do
+    property :is_paginated, :boolean, desc: 'Is response paginated or not.'
+    property :page, Integer, desc: 'Page returned.'
+    property :per_page, Integer, desc: 'Number of resources returned per page.'
+    property :evidence_variables, array_of: Hash do
+      property :id, Integer, desc: 'Resource ID.'
+      property :url, String, desc: 'URL of resource.'
+      property :name, String, desc: 'Resource name (human readable).'
+    end
+  end
   def index
     page     = params[:page]
     per_page = params[:per_page]
@@ -35,6 +45,40 @@ class Api::V2::EvidenceVariablesController < Api::V2::BaseController
 
   api :GET, '/v2/evidence_variables/:id.json', 'Individual Outcome Definition. FHIR formatted evidence variable. See http://build.fhir.org/evidencevariable.html for full description. Requires API Key.'
   param_group :resource_id, Api::V2::BaseController
+  returns desc: 'FHIR formatted Outcome Definition.' do
+    property :resourceType, String
+    property :id, Integer
+    property :meta, Hash do
+      property :versionId, Integer
+    end
+    property :url_index, String
+    property :url, String
+    property :name, String
+    property :title, String
+    property :status, String
+    property :date, DateTime
+    property :description, String
+    property :characteristic, array_of: Hash do
+      property :description, String
+      property :definitionCodeableConcept, Hash do
+        property :coding, array_of: Hash do
+          property :system, String
+          property :code, Integer
+          property :display, String
+        end
+        property :exclude, :boolean
+      end
+      property :timeFromStart, Hash do
+        property :quantity, Hash do
+          property :value, String
+          property :comparator, String
+          property :unit, String
+          property :system, String
+          property :code, String
+        end
+      end
+    end
+  end
   def show
     @populations = @evidence_variable.extractions_extraction_forms_projects_sections_type1_rows
     pop = @populations.try(:last)
