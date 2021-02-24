8f14e09d010cc89734ebd1574dd330df7ce3cb66
Jens Jap <jens_jap@brown.edu>
2021-02-23 22:48:55 -0800

    Adds a few more data fields for FHIR citation import.

--- app/jobs/import_jobs/json_import_job/_citation_fhir_importer.rb
+++ app/jobs/import_jobs/json_import_job/_citation_fhir_importer.rb
@@ -5,25 +5,61 @@ class CitationFhirImporter
   end
 
   def run
+    citation = nil
+    pmid = @citation_json.fetch("medlinePubMed", {}).fetch("pmid", nil) || fetch_pmid_from_identifiers
+
     citation_hash = {
       citation_type:     CitationType.find_by(name: "Primary"),
       name:              @citation_json.fetch("articleTitle", ""),
       refman:            "",
-      pmid:              @citation_json.fetch("medlinePubMed", {}).fetch("pmid", ""),
+      pmid:              pmid,
       abstract:          @citation_json.fetch("abstract", ""),
       page_number_start: @citation_json.fetch("pagination", {}).fetch("firstPage", ""),
-      page_number_end:   @citation_json.fetch("pagination", {}).fetch("lastPage", "")}
+      page_number_end:   @citation_json.fetch("pagination", {}).fetch("lastPage", "")
+    }
 
-    citation_by_pmid = Citation.find_by(pmid: @citation_json.fetch("medlinePubMed", {}).fetch("pmid", ""))
-    if citation_by_pmid
-      @project.citations << citation_by_pmid
-      return
+    journal_hash = {
+      volume: @citation_json.fetch("journal", {}).fetch("journalIssue", {}).fetch("volume", ""),
+      issue:  @citation_json.fetch("journal", {}).fetch("journalIssue", {}).fetch("issue", ""),
+      name:   @citation_json.fetch("journal", {}).fetch("title", {}),
+      publication_date: @citation_json.fetch("journal", {}).fetch("journalIssue", {}).fetch("publicationDate", {}).fetch("text", "")
+    }
+
+    citation = Citation.find_by(pmid: pmid) if pmid.present?
+    if citation.present?
+      @project.citations << citation
+    else
+      Citation.transaction do
+        citation = Citation.find_or_create_by(citation_hash)
+        @project.citations_projects << CitationsProject.create(
+          citation: citation,
+          project: @project)
+      end
     end
 
-    Citation.transaction do
-      @project.citations_projects << CitationsProject.create(
-        citation: Citation.find_or_create_by(citation_hash),
-        project: @project)
+    journal = Journal.find_or_create_by(journal_hash)
+    citation.journal = journal unless citation.journal.present?
+  end  # END def run
+
+  private
+
+    def fetch_pmid_from_identifiers
+      @citation_json["identifier"].each do |idx, identifier|
+        if identifier["system"].eql?("https://www.ncbi.nlm.nih.gov/pubmed/")
+          return identifier["value"]
+        end
+      end
+
+      return nil
     end
-  end
+
+    def fetch_doi_from_identifiers
+      @citation_json["identifier"].each do |idx, identifier|
+        if identifier["system"].eql?("https://doi.org")
+          return identifier["value"]
+        end
+      end
+
+      return nil
+    end
 end
