c08ecbabd996e98a76e63e9bcb81828881c8bcd3
Jens Jap <jens_jap@brown.edu>
2021-07-19 08:48:38 -0700

    Deal with Redis deprecation warning.

--- config/application.rb
+++ config/application.rb
@@ -30,3 +30,6 @@ if Rails.env.production?
     config.dsn = Rails.application.credentials.dig(:sentry)
   end
 end
+
+# Deal with Redis deprecation warning. This will be removed in 5.0
+Redis.exists_returns_integer = false
