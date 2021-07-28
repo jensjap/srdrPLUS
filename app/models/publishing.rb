729609311b1d196990ee09c2590ad1f61005ddee
Randy Jap <randyjap@users.noreply.github.com>
2021-07-29 00:56:36 +0800

    fix correct publishing date; limit front page of published projects t… (#266)
    * fix correct publishing date; limit front page of published projects to 25 projects
    
    * don't update project update_at date when creating publishings

--- app/models/publishing.rb
+++ app/models/publishing.rb
@@ -23,7 +23,7 @@ class Publishing < ApplicationRecord
 
   acts_as_paranoid column: :active, sentinel_value: true
 
-  belongs_to :publishable, polymorphic: true, touch: true
+  belongs_to :publishable, polymorphic: true
   belongs_to :user, inverse_of: :publishings
 
   has_one :approval, as: :approvable, dependent: :destroy
