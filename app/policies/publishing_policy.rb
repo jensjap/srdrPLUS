5be077a8dfa57a42396fa74148a73342cbc79dbb
Randy Jap <randyjap@gmail.com>
2021-08-05 14:41:00 +0800

    cwb-28: only allow project contributors to submit publishing requests

--- app/policies/publishing_policy.rb
+++ app/policies/publishing_policy.rb
@@ -1,16 +1,9 @@
 class PublishingPolicy < ApplicationPolicy
   def new?
-    @record = @record.publishable
     project_contributor?
   end
 
   def create?
-    @record = @record.publishable
-    project_contributor?
-  end
-
-  def destroy?
-    @record = @record.publishable
     project_contributor?
   end
 end
