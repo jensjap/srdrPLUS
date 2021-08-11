4acafd93c3d1c3fb1e59d69e6d22934b33782420
Jens Jap <jens_jap@brown.edu>
2021-08-10 23:41:02 -0700

    Fix default_url_options hash for testing and production.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,17 +36,18 @@ Rails.application.configure do
   end
 
   # Set default url options.
+  routes.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
   # Do care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set default url options. In production, :host should be set to the actual host of your application.
+  routes.default_url_options = { host: "https://srdrplus.ahrq.gov/" }
   config.action_mailer.default_url_options = { host: "https://srdrplus.ahrq.gov/" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
