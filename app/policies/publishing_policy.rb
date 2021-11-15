b179bb270fdf8ede8891896e23956fc7ce3990aa
Randy Jap <randyjap@users.noreply.github.com>
2021-11-15 17:23:35 +0800

    Allow admins to see all drafts 2 (#286)
    * admins can see all published projects
    
    * allow admins to see pages: comparison tool and request publication

--- app/policies/publishing_policy.rb
+++ app/policies/publishing_policy.rb
@@ -1,9 +1,9 @@
 class PublishingPolicy < ApplicationPolicy
   def new?
-    project_contributor?
+    project_contributor? || @user.admin?
   end
 
   def create?
-    project_contributor?
+    project_contributor? || @user.admin?
   end
 end
