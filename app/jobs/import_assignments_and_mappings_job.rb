aa8d0c3bf1a667fc2c0069a5829a70727ebf1518
Jens Jap <jens_jap@brown.edu>
2022-03-27 23:07:55 +0800

    Creating extractions and inserting type1 (except Outcomes) works.

--- app/jobs/import_assignments_and_mappings_job.rb
+++ app/jobs/import_assignments_and_mappings_job.rb
@@ -100,11 +100,6 @@ class ImportAssignmentsAndMappingsJob < ApplicationJob
     # we would not have continued.
     _process_workbook_citation_references_section(@wb_worksheets[:wcr].first)
     _process_assignments_and_mappings_section(@wb_worksheets[:aam].first)
-
-#    # Now process the rest...note that we skip :workbook_citation_references.
-#    @wb_worksheets.except(:workbook_citation_references).each do |ws_name, ws|
-#      _process_type1_sections(ws_name, ws.first)
-#    end  # @wb_worksheets.except(:workbook_citation_references).each do |ws_name, ws|
   end  # def _process_worksheets_data
 
   # Populate @wb_data under key :wcr.
@@ -186,7 +181,7 @@ class ImportAssignmentsAndMappingsJob < ApplicationJob
 
   def _build_header_index_lookup_dict(header_row)
     # Trust the database first in regards to the sections present.
-    @lsof_type1_names = @project
+    @lsof_type1_section_names = @project
       .extraction_forms_projects_sections
       .includes(:section)
       .joins(:extraction_forms_projects_section_type)
@@ -204,22 +199,22 @@ class ImportAssignmentsAndMappingsJob < ApplicationJob
       /Timepoint Unit/i ]
     re_match_targets = Regexp.union(match_targets)
 
-    @lsof_type1_names.each do |type1_name|
+    @lsof_type1_section_names.each do |type1_section_name|
       header_row.cells.each do |cell|
         case cell.value
-        when /#{ type1_name.singularize } Name/
-          @dict_header_index_lookup["#{ type1_name.singularize } Name"] = cell.column
-        when /#{ type1_name.singularize } Description/
-          @dict_header_index_lookup["#{ type1_name.singularize } Description"] = cell.column
-        when /#{ type1_name.singularize } Specific Measurement/
-          @dict_header_index_lookup["#{ type1_name.singularize } Specific Measurement"] = cell.column
-        when /#{ type1_name.singularize } Type/
-          @dict_header_index_lookup["#{ type1_name.singularize } Type"] = cell.column
+        when /#{ type1_section_name.singularize } Name/
+          @dict_header_index_lookup["#{ type1_section_name.singularize } Name"] = cell.column
+        when /#{ type1_section_name.singularize } Description/
+          @dict_header_index_lookup["#{ type1_section_name.singularize } Description"] = cell.column
+        when /#{ type1_section_name.singularize } Specific Measurement/
+          @dict_header_index_lookup["#{ type1_section_name.singularize } Specific Measurement"] = cell.column
+        when /#{ type1_section_name.singularize } Type/
+          @dict_header_index_lookup["#{ type1_section_name.singularize } Type"] = cell.column
         when re_match_targets
           @dict_header_index_lookup["#{ cell.value }"] = cell.column
         end  # case cell.value
       end  # header_row.cells.each do |cell|
-    end  # @lsof_type1_names.each do |type1_name|
+    end  # @lsof_type1_section_names.each do |type1_section_name|
   end  # def _build_header_index_lookup_dict(header_row)
 
   # All data is sorted into the following structure:
@@ -233,19 +228,19 @@ class ImportAssignmentsAndMappingsJob < ApplicationJob
   #         ...
   #   }}}
   def _sort_row_data(user_email, user_id, wb_cit_ref_id, row)
-    @lsof_type1_names.each do |t1_name|
+    @lsof_type1_section_names.each do |type1_section_name|
       @wb_data[:aam][user_email] = {} unless @wb_data[:aam].has_key?(user_email)
       @wb_data[:aam][user_email][wb_cit_ref_id] = {} unless @wb_data[:aam][user_email].has_key?(wb_cit_ref_id)
       @wb_data[:aam][user_email][wb_cit_ref_id][:wb_cit_ref_id] = wb_cit_ref_id unless @wb_data[:aam][user_email][wb_cit_ref_id].has_key?(:wb_cit_ref_id)
-      @wb_data[:aam][user_email][wb_cit_ref_id][t1_name] = [] unless @wb_data[:aam][user_email][wb_cit_ref_id].has_key?(t1_name)
+      @wb_data[:aam][user_email][wb_cit_ref_id][type1_section_name] = [] unless @wb_data[:aam][user_email][wb_cit_ref_id].has_key?(type1_section_name)
 
-      case t1_name
+      case type1_section_name
       when "Outcomes"
-        @wb_data[:aam][user_email][wb_cit_ref_id][t1_name] << [
-          row[@dict_header_index_lookup["#{ t1_name.singularize } Name"]]&.value,
-          row[@dict_header_index_lookup["#{ t1_name.singularize } Specific Measurement"]]&.value,
-          row[@dict_header_index_lookup["#{ t1_name.singularize } Type"]]&.value
-        ] unless row[@dict_header_index_lookup["#{ t1_name.singularize } Name"]]&.value.blank?
+        @wb_data[:aam][user_email][wb_cit_ref_id][type1_section_name] << [
+          row[@dict_header_index_lookup["#{ type1_section_name.singularize } Name"]]&.value,
+          row[@dict_header_index_lookup["#{ type1_section_name.singularize } Specific Measurement"]]&.value,
+          row[@dict_header_index_lookup["#{ type1_section_name.singularize } Type"]]&.value
+        ] unless row[@dict_header_index_lookup["#{ type1_section_name.singularize } Name"]]&.value.blank?
 
         # Population and Timepoint data is only applicable with Outcomes.
         @wb_data[:aam][user_email][wb_cit_ref_id]["Populations"] = [] unless @wb_data[:aam][user_email][wb_cit_ref_id].has_key?("Populations")
@@ -260,189 +255,140 @@ class ImportAssignmentsAndMappingsJob < ApplicationJob
         ] unless row[@dict_header_index_lookup["Timepoint Name"]]&.value.blank?
 
       else
-        @wb_data[:aam][user_email][wb_cit_ref_id][t1_name] << [
-          row[@dict_header_index_lookup["#{ t1_name.singularize } Name"]]&.value,
-          row[@dict_header_index_lookup["#{ t1_name.singularize } Description"]]&.value
-        ] unless row[@dict_header_index_lookup["#{ t1_name.singularize } Name"]]&.value.blank?
+        @wb_data[:aam][user_email][wb_cit_ref_id][type1_section_name] << [
+          row[@dict_header_index_lookup["#{ type1_section_name.singularize } Name"]]&.value,
+          row[@dict_header_index_lookup["#{ type1_section_name.singularize } Description"]]&.value
+        ] unless row[@dict_header_index_lookup["#{ type1_section_name.singularize } Name"]]&.value.blank?
 
       end
-    end  # @lsof_type1_names.each do |t1_name|
+    end  # @lsof_type1_section_names.each do |type1_section_name|
   end  # def _sort_row_data(user_email, user_id, wb_cit_ref_id, row)
 
-  #!!!
   def _insert_wb_data_into_db
-    debugger
-    @wb_data.each do |wb_datum|
-    end  # @wb_data.each do |wb_datum|
+    @wb_data[:aam].each do |user_email, dict_wb_citation_reference_ids|
+      dict_wb_citation_reference_ids.each do |wb_cit_ref_id, dict_type1_data|
+        _process_assignments_and_mappings_extraction_data(user_email, dict_type1_data)
+      end  # dict_wb_citation_reference_ids.each do |wb_cit_ref_id, dict_type1_data|
+    end  # @wb_data[:aam].each do |user_email, dict_wb_citation_reference_ids|
   end  # def _insert_wb_data_into_db
 
+  def _process_assignments_and_mappings_extraction_data(user_email, data)
+    user     = User.find_by(email: user_email)
+    citation = Citation.find_by(id: @wb_data[:wcr][data[:wb_cit_ref_id]]["citation_id"])
+    @lsof_type1_section_names.each do |type1_section_name|
+      case type1_section_name
+      when "Outcomes"
+        _insert_outcome_type1_data(user, citation, type1_section_name, data)
 
+      else
+        _insert_generic_type1_data(user, citation, type1_section_name, data)
 
+      end  # case type1_section_name
+    end  # @lsof_type1_section_names.each do |type1_section_name|
+  end  # def _process_assignments_and_mappings_extraction_data(user_email, data)
 
+  def _insert_outcome_type1_data(user, citation, type1_section_name, data)
+  end  # def _insert_outcome_type1_data(user, citation, type1_section_name, data)
 
-#  def _process_type1_sections(ws_name, ws)
-#    sheet_data = ws.sheet_data
-#    sheet_data.rows[1..-1].each do |row|
-#      user_email        = row[0]&.value
-#      lsof_citation_ids = row[1]&.value.to_s.split(',').map(&:to_i)
-#      type1_name        = row[2]&.value
-#      type1_description = row[3]&.value
-#
-#      # Additional processing for "Outcomes" section.
-#      if ws_name.eql?(:outcomes)
-#        outcome_type           = row[4]&.value
-#        population_name        = row[5]&.value
-#        population_description = row[6]&.value
-#        timepoint_name         = row[7]&.value
-#        timepoint_unit         = row[8]&.value
-#        successful, e = _process_row(
-#          ws_name,
-#          user_email,
-#          lsof_citation_ids,
-#          type1_name,
-#          type1_description,
-#          outcome_type,
-#          population_name,
-#          population_description,
-#          timepoint_name,
-#          timepoint_unit
-#        )
-#
-#      else
-#        successful, e = _process_row(
-#          ws_name,
-#          user_email,
-#          lsof_citation_ids,
-#          type1_name,
-#          type1_description
-#        )
-#      end  # if ws_name.eql?(:outcomes)
-#
-#      unless successful
-#        @dict_errors[:row_processing_errors].present? \
-#          ? @dict_errors[:row_processing_errors] << "Error: \"#{ e }\", row: [#{ user_email }, #{ lsof_citation_ids }], while processing row in worksheet: #{ ws_name }" \
-#          : @dict_errors[:row_processing_errors] = ["Error: \"#{ e }\", row: [#{ user_email }, #{ lsof_citation_ids }], while processing row in worksheet: #{ ws_name }"]
-#      end  # if successful
-#    end  # sheet_data.rows.each do |row|
-#  end  # def _process_outcomes_section
-#
-#  def _process_row(ws_name, email, wb_citation_ids, type1_name, type1_description,
-#    outcome_type=nil, population_name=nil, population_description=nil,
-#    timepoint_name=nil, timepoint_unit=nil)
-#
-#    user = User.find_by(email: email)
-#    return false, "Unable to retrieve user." unless user.present?
-#
-#    wb_citation_ids.each do |wb_citation_id|
-#      # Retrieve citation.
-#      citation = _retrieve_full_citation_record_from_db(wb_citation_id)
-#      return false, "Unable to retrieve citation for wb citation id #{ wb_citation_id }." unless citation.present?
-#
-#      # find_or_create extraction.
-#      extraction = _retrieve_extraction_record(user, citation)
-#      return false, "Unable to retrieve extraction for wb citation id #{ wb_citation_id }" unless extraction.present?
-#      # Toggle all KQs for the extraction to true.
-#      _toggle_true_all_kqs_for_extraction(extraction)
-#
-#      # find_or_create type 1.
-#      _add_type1_to_extraction(extraction, ws_name, type1_name, type1_description)
-#    end  # wb_citation_ids.each do |wb_citation_id|
-#
-#    return true, nil
-#  end  # def _process_row(email, wb_citation_ids, type1_name, ..., timepoint_unit=nil)
-#
-#  def _retrieve_full_citation_record_from_db(wb_citation_id)
-#    citation = nil
-#    wb_citation = @dict_citation_references[wb_citation_id]
-#    begin
-#      citation = Citation.find(wb_citation["citation_id"])
-#    rescue ActiveRecord::RecordNotFound=> exception
-#      citation = Citation.find_by(name: wb_citation["citation_name"])
-#    ensure
-#      return citation
-#    end
-#  end  # def _retrieve_full_citation_record_from_db(wb_citation_id)
-#
-#  def _retrieve_extraction_record(user, citation)
-#      # CitationsProject object should exist, otherwise it would not have been
-#      # part of the template.
-#      citations_project = CitationsProject.find_or_create_by(
-#        citation: citation,
-#        project: @project
-#      )
-#
-#      # Find ProjectsUsersRole with Contributor role.
-#      pu  = ProjectsUser.find_or_create_by(
-#        project: @project,
-#        user: user
-#      )
-#
-#      # If this user is not a Contributor then make him one.
-#      pur = ProjectsUsersRole.find_by(
-#        projects_user: pu,
-#        role: @@LEADER_ROLE
-#      )
-#      unless pur.present?
-#        pur = ProjectsUsersRole.find_or_create_by(
-#          projects_user: pu,
-#          role: @@CONTRIBUTOR_ROLE
-#        )
-#      end  # unless pur.present?
-#
-#      # Find or create Extraction.
-#      extraction = Extraction.find_or_create_by(
-#        project: @project,
-#        citations_project: citations_project,
-#        projects_users_role: pur,
-#        consolidated: false
-#      )
-#
-#      return extraction
-#  end  # def _retrieve_extraction_record(user_id, citation_id)
-#
-#  def _toggle_true_all_kqs_for_extraction(extraction)
-#    @project.key_questions_projects.each do |kqp|
-#      ExtractionsKeyQuestionsProjectsSelection.find_or_create_by(
-#        extraction: extraction,
-#        key_questions_project: kqp
-#      )
-#    end  # @project.key_quesetions_projects.each do |kqp|
-#  end  # def _toggle_true_all_kqs_for_extraction(extraction)
-#
-#  # Find appropriate EEFPS and add type1.
-#  def _add_type1_to_extraction(extraction, ws_name, type1_name, type1_description)
-#    efps = @project.extraction_forms_projects_sections.joins(:section).where(sections: { name: ws_name }).first
-#    eefps = ExtractionsExtractionFormsProjectsSection.find_by(extraction: extraction, extraction_forms_projects_section: efps)
-#    n_hash = {"extractions_extraction_forms_projects_sections_type1s_attributes"=>
-#               {"0"=>
-#                 {"type1_attributes"=>
-#                   {"name"=>type1_name, "description"=>type1_description}
-#                 }
-#               }
-#             }
-#
-#    if eefps.present?
-#      eefps.update(n_hash) unless ws_name.eql?(:outcomes)
-#    end
-#
-#
-#    # potentially add into type1s table
-#    # how do we add into suggestions table?
-#    # add into extractions_extraction_forms_projects_sections_type1s table
-#    # need insert into orderings table
-#
-#    # this is params sent to eefps controller:
-#    # Parameters: {
-#    # "utf8"=>"✓",
-#    # "authenticity_token"=>"Cgzup9N3RkJV1Zn9Q2GfPb+BdT195g4Rri0io4hL/gEFN6ZxZC5m3+fDW3MPKlGarj6rDZg4pFDPXWMKkC6sXA==",
-#    # "extractions_extraction_forms_projects_section"=>{
-#    #   "action"=>"work",
-#    #   "extractions_extraction_forms_projects_sections_type1s_attributes"=>{"0"=>{"type1_attributes"=>{"name"=>"testing", "description"=>"123"}}}
-#    # },
-#    # "id"=>"299951"}
-#
-#    # Arms efps: @project.extraction_forms_projects_sections.joins(:section).where(sections: { name: "arms" })
-#
-#  end  # def _add_type1_to_extraction(extraction, ws_name, type1_name, type1_description)
+  def _insert_generic_type1_data(user, citation, type1_section_name, data)
+    extraction = _retrieve_extraction_record(user, citation)
+    _toggle_true_all_kqs_for_extraction(extraction) if extraction.present?
+    data[type1_section_name].each do |lsof_type1_info|
+      # Reminder:
+      #   lsof_type1_info = [
+      #     ["Type 1 Name", "Type 1 Description"],
+      #     ["Type 2 Name", "Type 2 Description"],
+      #     ...]
+      _add_generic_type1_to_extraction(
+        extraction,
+        type1_section_name,
+        lsof_type1_info[0],
+        lsof_type1_info[1]) unless lsof_type1_info[0].blank?
+    end  # data[type1_section_name].each do |lsof_type1_info|
+  end  # def _insert_generic_type1_data(user, citation, type1_section_name, data)
 
+  def _retrieve_extraction_record(user, citation)
+      # CitationsProject may or not exist. Call find_or_create_by.
+      citations_project = CitationsProject.find_or_create_by(
+        citation: citation,
+        project: @project
+      )
+
+      # Find ProjectsUsersRole with Contributor role.
+      pu  = ProjectsUser.find_or_create_by(
+        project: @project,
+        user: user
+      )
+
+      # If this user is not a Contributor then make him one.
+      pur = ProjectsUsersRole.find_by(
+        projects_user: pu,
+        role: @@LEADER_ROLE
+      )
+      unless pur.present?
+        pur = ProjectsUsersRole.find_or_create_by(
+          projects_user: pu,
+          role: @@CONTRIBUTOR_ROLE
+        )
+      end  # unless pur.present?
+
+      # Find or create Extraction.
+      extraction = Extraction.find_or_create_by(
+        project: @project,
+        citations_project: citations_project,
+        projects_users_role: pur,
+        consolidated: false
+      )
+
+      return extraction
+  end  # def _retrieve_extraction_record(user, citation)
+
+  def _toggle_true_all_kqs_for_extraction(extraction)
+    @project.key_questions_projects.each do |kqp|
+      ExtractionsKeyQuestionsProjectsSelection.find_or_create_by(
+        extraction: extraction,
+        key_questions_project: kqp
+      )
+    end  # @project.key_quesetions_projects.each do |kqp|
+  end  # def _toggle_true_all_kqs_for_extraction(extraction)
+
+  # Find appropriate EEFPS and add type1.
+  def _add_generic_type1_to_extraction(extraction, type1_section_name, type1_name, type1_description)
+    efps = @project
+      .extraction_forms_projects_sections
+      .joins(:section)
+      .where(sections: { name: type1_section_name })
+      .first
+    eefps = ExtractionsExtractionFormsProjectsSection
+      .find_by(extraction: extraction, extraction_forms_projects_section: efps)
+    n_hash = {"extractions_extraction_forms_projects_sections_type1s_attributes"=>
+               {"0"=>
+                 {"type1_attributes"=>
+                   {"name"=>type1_name, "description"=>type1_description}
+                 }
+               }
+             }
+
+    if eefps.present?
+      eefps.update(n_hash) unless type1_section_name.eql?("Outcomes")
+    end
+
+    # potentially add into type1s table
+    # how do we add into suggestions table?
+    # add into extractions_extraction_forms_projects_sections_type1s table
+    # need insert into orderings table
+
+    # This is the params hash sent to eefps controller:
+    # Parameters: {
+    # "utf8"=>"✓",
+    # "authenticity_token"=>"Cgzup9N3RkJV1Zn9Q2GfPb+BdT195g4Rri0io4hL/gEFN6ZxZC5m3+fDW3MPKlGarj6rDZg4pFDPXWMKkC6sXA==",
+    # "extractions_extraction_forms_projects_section"=>{
+    #   "action"=>"work",
+    #   "extractions_extraction_forms_projects_sections_type1s_attributes"=>{"0"=>{"type1_attributes"=>{"name"=>"testing", "description"=>"123"}}}
+    # },
+    # "id"=>"299951"}
+
+    # Arms efps: @project.extraction_forms_projects_sections.joins(:section).where(sections: { name: "arms" })
+
+  end  # def _add_generic_type1_to_extraction(extraction, type1_section_name, type1_name, type1_description)
 end  # class ImportAssignmentsAndMappingsJob < ApplicationJob
