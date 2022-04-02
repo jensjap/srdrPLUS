75f415409d56c110ea906397e75abf13c13236e3
Jens Jap <jens@mycenaean.org>
2022-04-03 02:15:36 +0800

    Start fixing seed data.

--- test/seed_data_helper.rb
+++ test/seed_data_helper.rb
@@ -34,7 +34,8 @@ module SeedData
         { name: 'Distiller Section' },
         { name: 'Citation' },
         { name: 'Project' },
-        { name: 'Distiller' }
+        { name: 'Distiller' },
+        { name: 'Assignments and Mappings'}
       ])
 
       #FileType.
@@ -1224,7 +1225,7 @@ end
 
 # Sample Data.
 module SeedDataExtended
-  unless Rails.env.test?
+  if Rails.env.test?
     def self.extended(object)
       project_titles = [
         ['Definition of Treatment-Resistant Depression in the Medicare Population', 'The purpose of this technology assessment is to review the current definitions of treatment-resistant depression (TRD), to assess how closely current TRD treatment studies fit the most common definition, and to suggest how to improove TRD treatment research.'],
