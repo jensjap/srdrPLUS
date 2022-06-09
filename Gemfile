30323f3e364046a577e784f841ceed64e72b953b
Randy Jap <randyjap@gmail.com>
2022-06-09 09:57:05 +0800

    remove rails_admin

--- Gemfile
+++ Gemfile
@@ -1,170 +1,168 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails', '~> 6.1.6'
+gem "rails", "~> 6.1.6"
 
 # Use mysql as the database for Active Record
-gem 'mysql2'
+gem "mysql2"
 # Use Puma as the app server
-gem 'puma', '~> 5.6.4'
+gem "puma", "~> 5.6.4"
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+gem "sass-rails", "~> 5.0"
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem "uglifier", ">= 1.3.0"
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
+gem "coffee-rails", "~> 4.2"
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+gem "jquery-rails"
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.10.1'
+gem "jbuilder", "~> 2.10.1"
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'bcrypt', '~> 3.1.7'
+gem "bcrypt", "~> 3.1.7"
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
-  gem 'factory_bot_rails'
-  gem 'rspec-rails'
+  gem "byebug", platform: :mri
+  gem "factory_bot_rails"
+  gem "rspec-rails"
   gem "rspec_junit_formatter"
-  gem 'dotenv-rails'
+  gem "dotenv-rails"
 end
 
 group :test do
-  gem 'minitest-rails-capybara'
-  gem 'simplecov', require: false
-  gem 'database_cleaner-active_record'
+  gem "minitest-rails-capybara"
+  gem "simplecov", require: false
+  gem "database_cleaner-active_record"
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'web-console', '~> 3.7.0'
-  gem 'guard'
-  gem 'guard-minitest'
-  gem 'listen', '~> 3.0.5'
+  gem "web-console", "~> 3.7.0"
+  gem "guard"
+  gem "guard-minitest"
+  gem "listen", "~> 3.0.5"
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem "spring"
+  gem "spring-watcher-listen", "~> 2.0.0"
   # Annotate Models
-  gem 'annotate'
-  gem 'bullet'
+  gem "annotate"
+  gem "bullet"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 ## Non-default gems.
 
 # Use Zurb Foundation as Front-End Framework
-gem 'foundation-rails'
-gem 'autoprefixer-rails'
-gem 'foundation-icons-sass-rails'
+gem "foundation-rails"
+gem "autoprefixer-rails"
+gem "foundation-icons-sass-rails"
 
 # Prettier templates.
-gem 'slim-rails'
+gem "slim-rails"
 
 # Simplified forms.
-gem 'simple_form'
-gem 'country_select'
-gem 'cocoon'
+gem "simple_form"
+gem "country_select"
+gem "cocoon"
 
 # Authentication.
-gem 'devise'
-gem 'omniauth'
-gem 'remotipart', github: 'mshibuya/remotipart'
-# gem 'rails_admin', '~> 3.0'
+gem "devise"
+gem "omniauth"
+gem "remotipart", github: "mshibuya/remotipart"
 
 # Flash messages.
-gem 'toastr_rails'
+gem "toastr_rails"
 
 # Versioning of models + soft-delete.
-gem 'paranoia', '~>2.2'
+gem "paranoia", "~>2.2"
 
 # Create lots of data.
-gem 'faker', github: 'stympy/faker'
+gem "faker", github: "stympy/faker"
 
 # Access ruby data in JavaScript.
-gem 'gon'
+gem "gon"
 
 # Pagination.
-gem 'kaminari'
+gem "kaminari"
 
 # CORS.
-gem 'rack-cors'
+gem "rack-cors"
 
 # Help DRY up code.
-gem 'responders'
+gem "responders"
 
 # Api documentation.
-gem 'apipie-rails'
+gem "apipie-rails"
 
 # Background jobs.
-gem 'sidekiq', '6.4.0'
+gem "sidekiq", "6.4.0"
 
 # Spreadsheet generation.
-gem 'caxlsx'
-gem 'caxlsx_rails'
+gem "caxlsx"
+gem "caxlsx_rails"
 
 # Spreadsheet reading.
-gem 'rubyXL', '~> 1.2.10'
-gem 'zip-zip'
+gem "rubyXL", "~> 1.2.10"
+gem "zip-zip"
 
 # Searching with Elasticsearch.
-gem 'searchkick'
-gem 'searchjoy'
+gem "searchkick"
+gem "searchjoy"
 
-gem 'passenger'
+gem "passenger"
 
 # bioruby for pubmed queries.
-gem 'bio'
+gem "bio"
 
 # for parsing ris files.
-gem 'ref_parsers', '~> 0.2.0'
+gem "ref_parsers", "~> 0.2.0"
 
 # breadcrumbs.
-gem 'breadcrumbs_on_rails'
+gem "breadcrumbs_on_rails"
 
 # authorizations.
-gem 'pundit'
+gem "pundit"
 
 # List reordering, Drag & Drop.
-gem 'sortable-rails'
+gem "sortable-rails"
 
 # Simple calls to external API.
-gem 'httparty'
+gem "httparty"
 
 # New for Rails 5.2.
-gem 'bootsnap'
+gem "bootsnap"
 
 # Access to AWS S3 Cloud Storage.
-gem 'aws-sdk-s3', require: false
+gem "aws-sdk-s3", require: false
 
 # Access Google sheets programmatically
-gem 'google-api-client'
+gem "google-api-client"
 
 # Allows periodic background jobs
-gem 'sidekiq-cron', '~> 1.1'
+gem "sidekiq-cron", "~> 1.1"
 
 # For making sortable searchable tables
-gem 'jquery-datatables'
+gem "jquery-datatables"
 
 # Formatted console logs
-gem 'awesome_print'
+gem "awesome_print"
 
 # Fuzzy Match
-gem 'fuzzy_match'
+gem "fuzzy_match"
 
 # Font Awesome Icons
 #gem 'font-awesome-rails'
@@ -189,15 +187,15 @@ gem "sentry-sidekiq"
 # gem 'newrelic-infinite_tracing'
 
 # Limit request rates
-gem 'rack-attack'
+gem "rack-attack"
 
 # gem 'rack-mini-profiler'
 
-gem 'foreman'
+gem "foreman"
 
-gem 'ahoy_matey'
+gem "ahoy_matey"
 
 # Easy maintenance mode.
-gem 'turnout'
+gem "turnout"
 
 # gem 'sassc-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
-require 'sidekiq/web'
+require "sidekiq/web"
 
 Rails.application.routes.draw do
-  get 'public_data', to: 'public_data#show'
+  get "public_data", to: "public_data#show"
 
   resources :publishings, only: [:new, :create, :destroy]
-  post 'publishings/:id/approve', to: 'publishings#approve', as: 'publishings_approve'
-  post 'publishings/:id/rescind_approval', to: 'publishings#rescind_approval', as: 'rescind_approval'
+  post "publishings/:id/approve", to: "publishings#approve", as: "publishings_approve"
+  post "publishings/:id/rescind_approval", to: "publishings#rescind_approval", as: "rescind_approval"
 
   resources :project_report_links, only: [:index, :view] do
-    get 'new_query_form'
-    post 'options_form'
+    get "new_query_form"
+    post "options_form"
   end
 
   resources :searches, only: [:index]
@@ -19,31 +19,29 @@ Rails.application.routes.draw do
   resources :sd_search_databases, only: [:index]
   resources :key_questions, only: [:index]
   resources :sd_key_questions, only: [:index] do
-    get 'destroy_with_picodts', on: :member
+    get "destroy_with_picodts", on: :member
   end
   resources :sd_meta_data_queries, only: [:create, :update, :destroy]
   resources :review_types, only: [:index]
   resources :data_analysis_levels, only: [:index]
 
-
   devise_for :admins
   devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    unlocks: 'users/unlocks'
-  }
+            confirmations: "users/confirmations",
+            omniauth_callbacks: "users/omniauth_callbacks",
+            passwords: "users/passwords",
+            registrations: "users/registrations",
+            sessions: "users/sessions",
+            unlocks: "users/unlocks",
+          }
 
   devise_scope :user do
-    post '/users/api_key_reset' => 'users/registrations#api_key_reset'
+    post "/users/api_key_reset" => "users/registrations#api_key_reset"
   end
 
   authenticate :admin do
-    # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     mount Searchjoy::Engine, at: "searchjoy"
-    mount Sidekiq::Web => '/sidekiq'
+    mount Sidekiq::Web => "/sidekiq"
   end
 
   apipie
@@ -76,34 +74,34 @@ Rails.application.routes.draw do
 
       resources :citations, only: [:index] do
         collection do
-          get 'titles'
-          get 'project_citations_query'
+          get "titles"
+          get "project_citations_query"
         end
       end
 
       resources :projects, shallow: true do
         resources :projects_users_roles, only: [:index]
         resources :assignments do
-          get 'screen', on: :member
-          get 'history', on: :member
+          get "screen", on: :member
+          get "history", on: :member
         end
         resources :citations, only: [:index] do
           collection do
-            get 'labeled'
-            get 'unlabeled'
+            get "labeled"
+            get "unlabeled"
           end
         end
         resources :extractions, only: [:index]
         resources :extraction_forms_projects do
           resources :extraction_forms_projects_sections, only: [:index, :show] do
-            post 'toggle_hiding', to: 'extraction_forms_projects_sections#toggle_hiding'
+            post "toggle_hiding", to: "extraction_forms_projects_sections#toggle_hiding"
             resources :type1s, only: [:index]
           end
         end
       end
       resources :orderings do
         collection do
-          patch 'update_positions'
+          patch "update_positions"
         end
       end
     end  # END namespace :v1 do
@@ -111,7 +109,7 @@ Rails.application.routes.draw do
     namespace :v2 do
       resources :mesh_descriptors, only: [:index]
       resources :evidence_variables, only: [:index, :show]
-      get :public_projects, to: 'projects#public_projects'
+      get :public_projects, to: "projects#public_projects"
       resources :projects, shallow: true do
         member do
           post :import_citations_fhir_json
@@ -127,7 +125,7 @@ Rails.application.routes.draw do
   end  # END namespace :api do
 
   resources :assignments do
-    get 'screen', on: :member
+    get "screen", on: :member
   end
   resources :authors
   resources :comparisons
@@ -140,7 +138,7 @@ Rails.application.routes.draw do
   resources :statusings, only: [:update]
 
   concern :paginatable do
-    get '(page/:page)', action: :index, on: :collection, as: ''
+    get "(page/:page)", action: :index, on: :collection, as: ""
   end
 
   concern :invitable do
@@ -148,7 +146,7 @@ Rails.application.routes.draw do
   end
 
   resources :extractions_extraction_forms_projects_sections_type1s, only: [] do
-    get 'get_results_populations', on: :member
+    get "get_results_populations", on: :member
   end
 
   resources :pictures, only: [:create] do
@@ -156,12 +154,12 @@ Rails.application.routes.draw do
   end
 
   resources :sd_meta_data, only: [] do
-    get 'preview'
-    post 'section_update'
-    post 'mapping_update'
+    get "preview"
+    post "section_update"
+    post "mapping_update"
   end
 
-  get 'sd_key_questions/:id/fuzzy_match', to: 'sd_key_questions#fuzzy_match'
+  get "sd_key_questions/:id/fuzzy_match", to: "sd_key_questions#fuzzy_match"
 
   resources :data_audits, only: [:index, :update]
   resources :projects, concerns: :paginatable, shallow: true do
@@ -169,60 +167,56 @@ Rails.application.routes.draw do
     resources :sd_meta_data
     resources :teams, concerns: :invitable, only: [:create, :update, :destroy]
     member do
-      get  'confirm_deletion'
-      get  'next_assignment'
-      post 'import_csv'
-      post 'import_ris'
-      post 'import_endnote'
-      post 'import_pubmed'
-      post 'dedupe_citations'
-      post 'create_citation_screening_extraction_form'
-      post 'create_full_text_screening_extraction_form'
+      get "confirm_deletion"
+      get "next_assignment"
+      post "import_csv"
+      post "import_ris"
+      post "import_endnote"
+      post "import_pubmed"
+      post "dedupe_citations"
+      post "create_citation_screening_extraction_form"
+      post "create_full_text_screening_extraction_form"
     end
 
     resources :citations do
       collection do
-        get 'labeled'
-        get 'unlabeled'
+        get "labeled"
+        get "unlabeled"
       end
     end
 
     resources :screening_options
     resources :tasks
     resources :extractions do
-
       collection do
-        get 'comparison_tool'
-        get 'consolidate'
-        get 'edit_type1_across_extractions'
+        get "comparison_tool"
+        get "consolidate"
+        get "edit_type1_across_extractions"
       end
 
       member do
-        get 'work'
-        put 'update_kqp_selections'
-        get 'change_outcome_in_results_section', constraints: { format: 'js' }
+        get "work"
+        put "update_kqp_selections"
+        get "change_outcome_in_results_section", constraints: { format: "js" }
       end
 
       resources :extractions_extraction_forms_projects_sections do
         resources :extractions_extraction_forms_projects_sections_question_row_column_fields, only: [:update]
         resources :extractions_extraction_forms_projects_sections_type1s, only: [:edit, :update, :destroy] do
-
           member do
-            get 'edit_timepoints'
-            get 'edit_populations'
+            get "edit_timepoints"
+            get "edit_populations"
           end
 
           resources :extractions_extraction_forms_projects_sections_type1_rows, only: [:create] do
             resources :extractions_extraction_forms_projects_sections_type1_row_columns, only: [:create] do
               resources :result_statistic_sections, only: [:edit, :update] do
-
                 member do
-                  post 'add_comparison'
-                  delete 'remove_comparison'
-                  get  'consolidate'
-                  get  'manage_measures', constraints: { format: 'js' }
+                  post "add_comparison"
+                  delete "remove_comparison"
+                  get "consolidate"
+                  get "manage_measures", constraints: { format: "js" }
                 end
-
               end
             end
           end
@@ -230,23 +224,21 @@ Rails.application.routes.draw do
       end
     end
     resources :extraction_forms_projects, only: [:create, :edit, :update, :destroy] do
-      get 'build', on: :member
+      get "build", on: :member
       resources :extraction_forms_projects_sections, only: [:new, :create, :edit, :update, :destroy] do
-
         resources :extraction_forms_projects_sections_type1s, only: [:edit, :update]
 
         member do
-          post 'add_quality_dimension'
+          post "add_quality_dimension"
         end
 
         resources :questions, only: [:new, :create, :edit, :update, :destroy] do
-
           member do
-            patch 'toggle_dependency'
-            post  'add_column'
-            post  'add_row'
-            get   'dependencies'
-            post 'duplicate'
+            patch "toggle_dependency"
+            post "add_column"
+            post "add_row"
+            get "dependencies"
+            post "duplicate"
             #post 'duplicate'
           end
 
@@ -255,8 +247,8 @@ Rails.application.routes.draw do
               resources :question_row_columns_question_row_column_options, only: [:destroy]
 
               member do
-                get 'answer_choices'
-                delete 'destroy_entire_column'
+                get "answer_choices"
+                delete "destroy_entire_column"
               end
 
               resources :question_row_column_fields, only: [] do
@@ -266,36 +258,36 @@ Rails.application.routes.draw do
           end
         end
         resources :type1s, only: [:new, :create, :edit, :update, :destroy]
-        delete 'dissociate_type1'
+        delete "dissociate_type1"
       end
     end
     resources :key_questions_projects, only: [:create, :edit, :update, :destroy]
 
     collection do
-      get 'filter'
+      get "filter"
     end
 
     member do
-      post 'export'
-      post 'export_citation_labels'
-      post 'export_to_gdrive'
+      post "export"
+      post "export_citation_labels"
+      post "export_to_gdrive"
     end
 
     resources :imports, only: [:new]
   end
 
-  root to: 'static_pages#home'
+  root to: "static_pages#home"
 
-  get 'about'              => 'static_pages#about'
-  get 'citing'             => 'static_pages#citing'
-  get 'contact'            => 'static_pages#contact'
-  get 'help'               => 'static_pages#help'
-  get 'usage'              => 'static_pages#usage'
-  get 'blog'               => 'static_pages#blog'
-  get 'resources'          => 'static_pages#resources'
-  get 'published_projects' => 'static_pages#published_projects'
+  get "about" => "static_pages#about"
+  get "citing" => "static_pages#citing"
+  get "contact" => "static_pages#contact"
+  get "help" => "static_pages#help"
+  get "usage" => "static_pages#usage"
+  get "blog" => "static_pages#blog"
+  get "resources" => "static_pages#resources"
+  get "published_projects" => "static_pages#published_projects"
 
-  resource  :profile, only: [:show, :edit, :update]
+  resource :profile, only: [:show, :edit, :update]
   resources :degrees, only: [:index]
   resources :organizations, only: [:index]
   resources :sections, only: [:index]
