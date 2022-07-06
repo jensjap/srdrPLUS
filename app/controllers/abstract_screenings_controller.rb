541777ac761c048b8666b57f3a527aca580c6f93
Randy Jap <randyjap@gmail.com>
2022-07-06 13:19:48 +0800

    reorder actions

--- app/controllers/abstract_screenings_controller.rb
+++ app/controllers/abstract_screenings_controller.rb
@@ -5,20 +5,6 @@ class AbstractScreeningsController < ApplicationController
   before_action :set_project, only: %i[index new create citation_lifecycle_management]
   before_action :set_abstract_screening, only: %i[create_word_weight label]
 
-  def index
-    prepare_pipeline_stats
-    @abstract_screenings =
-      @project
-      .abstract_screenings
-      .order(id: :desc)
-      .page(params[:page])
-      .per(5)
-  end
-
-  def new
-    @abstract_screening = @project.abstract_screenings.new
-  end
-
   def create
     @abstract_screening =
       @project.abstract_screenings.new(abstract_screening_params)
@@ -32,14 +18,66 @@ class AbstractScreeningsController < ApplicationController
     end
   end
 
-  def show
+  def create_word_weight
+    weight = params[:weight]
+    word = params[:word].downcase
+
+    ww = WordWeight.find_or_initialize_by(word:, abstract_screenings_projects_users_role:)
+    ww.update(weight:)
+    render json: abstract_screenings_projects_users_role.word_weights_object
+  end
+
+  def citation_lifecycle_management
+    prepare_pipeline_stats
+    @citations = @project.citations.page(params[:page]).per(10)
+  end
+
+  def destroy
     @abstract_screening = AbstractScreening.find(params[:id])
-    @abstract_screening_results =
-      @abstract_screening
-      .abstract_screening_results
-      .order(created_at: :desc)
+    @abstract_screening.destroy
+    redirect_to project_abstract_screenings_path(@abstract_screening.project)
+  end
+
+  def index
+    prepare_pipeline_stats
+    @abstract_screenings =
+      @project
+      .abstract_screenings
+      .order(id: :desc)
       .page(params[:page])
       .per(5)
+  end
+
+  def label
+    payload = label_params[:data]
+    @label = payload[:label_value]
+
+    @abstract_screening_result =
+      AbstractScreeningResult.find_by(id: payload[:abstract_screening_result_id])
+    if @abstract_screening_result
+      @abstract_screening_result.update(label: @label) if @label
+      abstract_screenings_projects_users_role
+        .process_reasons(@abstract_screening_result, payload[:predefined_reasons],
+                         payload[:custom_reasons])
+      abstract_screenings_projects_users_role
+        .process_tags(@abstract_screening_result, payload[:predefined_tags],
+                      payload[:custom_tags])
+      @abstract_screening_result&.note&.really_destroy!
+      @abstract_screening_result.create_note!(
+        value: payload[:notes],
+        projects_users_role: abstract_screenings_projects_users_role.projects_users_role
+      )
+      @abstract_screenings_citations_project = @abstract_screening_result.abstract_screenings_citations_project
+      @random_citation = @abstract_screening_result.citation
+    end
+
+    get_next_abstraction_screening_result if @abstract_screening_result.nil? || @label.present?
+
+    render_label_json_data
+  end
+
+  def new
+    @abstract_screening = @project.abstract_screenings.new
   end
 
   def rescreen
@@ -72,35 +110,67 @@ class AbstractScreeningsController < ApplicationController
     render layout: 'abstrackr'
   end
 
-  def label
-    payload = label_params[:data]
-    label = payload[:label_value]
-
-    @abstract_screening_result =
-      AbstractScreeningResult.find_by(id: payload[:abstract_screening_result_id])
-    if @abstract_screening_result
-      @abstract_screening_result.update(label:) if label
-      abstract_screenings_projects_users_role
-        .process_reasons(@abstract_screening_result, payload[:predefined_reasons],
-                         payload[:custom_reasons])
-      abstract_screenings_projects_users_role
-        .process_tags(@abstract_screening_result, payload[:predefined_tags],
-                      payload[:custom_tags])
-      @abstract_screening_result&.note&.really_destroy!
-      @abstract_screening_result.create_note!(
-        value: payload[:notes],
-        projects_users_role: abstract_screenings_projects_users_role.projects_users_role
-      )
-      @abstract_screenings_citations_project = @abstract_screening_result.abstract_screenings_citations_project
-      @random_citation = @abstract_screening_result.citation
-    end
-
-    label_preparations if label || @abstract_screening_result.nil?
-
-    render_label_json_data
+  def show
+    @abstract_screening = AbstractScreening.find(params[:id])
+    @abstract_screening_results =
+      @abstract_screening
+      .abstract_screening_results
+      .order(created_at: :desc)
+      .page(params[:page])
+      .per(5)
   end
 
-  def label_preparations
+  private
+
+  def abstract_screening_params
+    strong_params = params.require(:abstract_screening).permit(
+      :abstract_screening_type,
+      :yes_tag_required,
+      :no_tag_required,
+      :maybe_tag_required,
+      :yes_reason_required,
+      :no_reason_required,
+      :maybe_reason_required,
+      :yes_note_required,
+      :no_note_required,
+      :maybe_note_required,
+      :only_predefined_reasons,
+      :only_predefined_tags,
+      :hide_author,
+      :hide_journal,
+      projects_users_role_ids: [],
+      reason_ids: [],
+      tag_ids: []
+    )
+    allow_new_tags(strong_params)
+    allow_new_reasons(strong_params)
+  end
+
+  def abstract_screenings_projects_users_role
+    AbstractScreeningsProjectsUsersRole.find_aspur(current_user, @abstract_screening)
+  end
+
+  def allow_new_reasons(strong_params)
+    return strong_params unless strong_params['reason_ids'].present?
+
+    reasons = strong_params['reason_ids']
+    strong_params['reason_ids'] = reasons.map do |reason|
+      reason == '' || reason[0] != '_' ? reason : Reason.find_or_create_by(name: reason[1..]).id
+    end
+    strong_params
+  end
+
+  def allow_new_tags(strong_params)
+    return strong_params unless strong_params['tag_ids'].present?
+
+    tags = strong_params['tag_ids']
+    strong_params['tag_ids'] = tags.map do |tag|
+      tag == '' || tag[0] != '_' ? tag : Tag.find_or_create_by(name: tag[1..]).id
+    end
+    strong_params
+  end
+
+  def get_next_abstraction_screening_result
     @asr_id = session[:abstract_screening_result_id]
     if @asr_id
       session.delete(:abstract_screening_result_id)
@@ -156,6 +226,25 @@ class AbstractScreeningsController < ApplicationController
                  abstract_screenings_projects_users_role:)
       @random_citation = @abstract_screening_result.citation
     end
+  end
+
+  def label_params
+    params
+      .permit(
+        data: [
+          :label_value, :notes, :abstract_screening_result_id, :rescreen, {
+            predefined_reasons: {}, custom_reasons: {}, predefined_tags: {},
+            custom_tags: {}, citation: [:abstract_screenings_citations_project_id]
+          }
+        ]
+      )
+  end
+
+  def prepare_pipeline_stats
+    @up = @project.citations_projects.where(citations_projects: { screening_status: nil })
+    @as = @project.citations_projects.where(citations_projects: { screening_status: 'AS' })
+    @fs = @project.citations_projects.where(citations_projects: { screening_status: 'FS' })
+    @de = @project.citations_projects.where(citations_projects: { screening_status: 'DE' })
   end
 
   def render_label_json_data
@@ -218,100 +307,11 @@ class AbstractScreeningsController < ApplicationController
     }
   end
 
-  def create_word_weight
-    weight = params[:weight]
-    word = params[:word].downcase
-
-    ww = WordWeight.find_or_initialize_by(word:, abstract_screenings_projects_users_role:)
-    ww.update(weight:)
-    render json: abstract_screenings_projects_users_role.word_weights_object
-  end
-
-  def destroy
-    @abstract_screening = AbstractScreening.find(params[:id])
-    @abstract_screening.destroy
-    redirect_to project_abstract_screenings_path(@abstract_screening.project)
-  end
-
-  def citation_lifecycle_management
-    prepare_pipeline_stats
-    @citations = @project.citations.page(params[:page]).per(10)
-  end
-
-  private
-
-  def abstract_screenings_projects_users_role
-    AbstractScreeningsProjectsUsersRole.find_aspur(current_user, @abstract_screening)
-  end
-
-  def set_project
-    @project = Project.includes(citations_projects: :citation).find(params[:project_id])
-  end
-
   def set_abstract_screening
     @abstract_screening = AbstractScreening.find(params[:abstract_screening_id])
   end
 
-  def label_params
-    params
-      .permit(
-        data: [
-          :label_value, :notes, :abstract_screening_result_id, :rescreen, {
-            predefined_reasons: {}, custom_reasons: {}, predefined_tags: {},
-            custom_tags: {}, citation: [:abstract_screenings_citations_project_id]
-          }
-        ]
-      )
-  end
-
-  def abstract_screening_params
-    strong_params = params.require(:abstract_screening).permit(
-      :abstract_screening_type,
-      :yes_tag_required,
-      :no_tag_required,
-      :maybe_tag_required,
-      :yes_reason_required,
-      :no_reason_required,
-      :maybe_reason_required,
-      :yes_note_required,
-      :no_note_required,
-      :maybe_note_required,
-      :only_predefined_reasons,
-      :only_predefined_tags,
-      :hide_author,
-      :hide_journal,
-      projects_users_role_ids: [],
-      reason_ids: [],
-      tag_ids: []
-    )
-    allow_new_tags(strong_params)
-    allow_new_reasons(strong_params)
-  end
-
-  def allow_new_tags(strong_params)
-    return strong_params unless strong_params['tag_ids'].present?
-
-    tags = strong_params['tag_ids']
-    strong_params['tag_ids'] = tags.map do |tag|
-      tag == '' || tag[0] != '_' ? tag : Tag.find_or_create_by(name: tag[1..]).id
-    end
-    strong_params
-  end
-
-  def allow_new_reasons(strong_params)
-    return strong_params unless strong_params['reason_ids'].present?
-
-    reasons = strong_params['reason_ids']
-    strong_params['reason_ids'] = reasons.map do |reason|
-      reason == '' || reason[0] != '_' ? reason : Reason.find_or_create_by(name: reason[1..]).id
-    end
-    strong_params
-  end
-
-  def prepare_pipeline_stats
-    @up = @project.citations_projects.where(citations_projects: { screening_status: nil })
-    @as = @project.citations_projects.where(citations_projects: { screening_status: 'AS' })
-    @fs = @project.citations_projects.where(citations_projects: { screening_status: 'FS' })
-    @de = @project.citations_projects.where(citations_projects: { screening_status: 'DE' })
+  def set_project
+    @project = Project.includes(citations_projects: :citation).find(params[:project_id])
   end
 end
