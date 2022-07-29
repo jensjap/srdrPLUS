7a761b785653b4eb36ce7f42c2fa5e33e3817731
sleepwalk712 <sleepwalk712@gmail.com>
2022-07-29 05:41:20 +0000

    Containerize application
    - Use docker compose to initialize apps
    - Seperate services into apps, sidekiq, mysql, redis, elasticsearch
    - Change .env file if needed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,16 @@
+CITATION_BATCH_SIZE = 1.freeze
+#CITATION_BATCH_SIZE = 1000.freeze
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDIS_SERVER_URL"] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_SERVER_URL"] }
+end
+
 schedule_file = "config/schedule.yml"
 
 if File.exist?(schedule_file) && Sidekiq.server?
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
-
-CITATION_BATCH_SIZE = 1.freeze
-#CITATION_BATCH_SIZE = 1000.freeze
