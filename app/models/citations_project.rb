3bb2289d39502de1159acd9b43b3d0576cf2f27c
Randy Jap <randyjap@gmail.com>
2022-08-10 15:01:49 +0800

    update annotations

--- app/models/citations_project.rb
+++ app/models/citations_project.rb
@@ -11,6 +11,7 @@
 #  updated_at        :datetime         not null
 #  consensus_type_id :integer
 #  pilot_flag        :boolean
+#  screening_status  :string(255)      default("CP"), not null
 #
 
 class CitationsProject < ApplicationRecord
