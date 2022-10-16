10e97b0134cbd243445868a6e0b7b1a7d1871719
sleepwalk712 <sleepwalk712@gmail.com>
2022-10-16 13:04:01 +0000

    Fix sidekiq crash problem

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,6 @@
 CITATION_BATCH_SIZE = 1.freeze
 #CITATION_BATCH_SIZE = 1000.freeze
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDIS_SERVER_URL"] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDIS_SERVER_URL"] }
-end
-
 schedule_file = "config/schedule.yml"
 
 Sidekiq.configure_server do |config|
