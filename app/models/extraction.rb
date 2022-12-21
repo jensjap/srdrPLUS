b71a0cfaca374543d3eb4031db43df6ae30b074c
Randy Jap <randyjap@gmail.com>
2022-12-21 16:45:46 +0800

    update annotations

--- app/models/extraction.rb
+++ app/models/extraction.rb
@@ -9,6 +9,7 @@
 #  consolidated           :boolean          default(FALSE)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  user_id                :integer
 #
 
 class Extraction < ApplicationRecord
