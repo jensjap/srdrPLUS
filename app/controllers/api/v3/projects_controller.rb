1d15ef47d33379a561cbafbc46cc2df14ddd11d5
sleepwalk712 <sleepwalk712@gmail.com>
2023-02-07 10:11:28 +0000

    Add fhir project policy

--- app/controllers/api/v3/projects_controller.rb
+++ app/controllers/api/v3/projects_controller.rb
@@ -1,15 +1,21 @@
 class Api::V3::ProjectsController < Api::V3::BaseController
 
   def show
-    @project = AllResourceSupplyingService.new.find_by_project_id(params[:id])
+    authorize(@project, policy_class: ProjectPolicy)
+    @project_bundle = AllResourceSupplyingService.new.find_by_project_id(@project.id)
     respond_to do |format|
-      format.fhir_xml { render xml: @project }
-      format.fhir_json { render json: @project }
-      format.html { render json: @project }
-      format.json { render json: @project }
-      format.xml { render xml: @project }
+      format.fhir_xml { render xml: @project_bundle }
+      format.fhir_json { render json: @project_bundle }
+      format.html { render json: @project_bundle }
+      format.json { render json: @project_bundle }
+      format.xml { render xml: @project_bundle }
       format.all { render text: 'Only HTML, JSON and XML are currently supported', status: 406 }
     end
   end
 
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
 end
