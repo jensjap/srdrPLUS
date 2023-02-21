7fdedfffec82dd87ec1e6693a3c36c51fbd9ee33
Jens Jap <jens@mycenaean.org>
2023-02-21 18:44:48 +0800

    Bug: Fix regression with Citation.authors field

--- app/controllers/extractions_controller.rb
+++ app/controllers/extractions_controller.rb
@@ -22,7 +22,7 @@ class ExtractionsController < ApplicationController
       .includes(
         [
           { user: :profile },
-          { citations_project: { citation: [:journal, :authors, { authors_citations: :ordering }] } },
+          { citations_project: { citation: %i[journal] } },
           { extractions_extraction_forms_projects_sections: [
             :status,
             { extraction_forms_projects_section: :section }
