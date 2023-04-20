50c8ebd3b4c83ad55d4d08ab8afb7bb8e5a10835
Jens Jap <jens@mycenaean.org>
2023-04-20 12:13:08 +0800

    Security: Tighten CORS origin requirement.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins(Rails.env.production? ? 'https://srdrplus.ahrq.gov' : '*')
 
     resource '*',
       headers: :any,
