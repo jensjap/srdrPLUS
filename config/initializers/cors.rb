38ad2efc3c4631a684c79cbf9c7a4a51e0409f0a
Jens Jap <jens@mycenaean.org>
2023-04-20 15:46:47 +0800

    Bugfix: More CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(Rails.env.production? ? 'https://srdrplus.ahrq.gov' : '*')
+    origins(Rails.env.production? ? 'https://srdrplus.ahrq.gov:443' : '*')
 
     resource '*',
       headers: :any,
