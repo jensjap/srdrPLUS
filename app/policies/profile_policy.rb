3b9a962f066d77065b2f98f211090b3838ce10e4
Jens Jap <jens@mycenaean.org>
2023-04-20 15:46:24 +0800

    Bugfix: Fix profile policy

--- app/policies/profile_policy.rb
+++ app/policies/profile_policy.rb
@@ -1,13 +1,20 @@
 class ProfilePolicy < ApplicationPolicy
+  def initialize(user, record)
+    raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+
+    @user = user
+    @record = record
+  end
+
   def show?
-    record.user == user
+    record.user.eql?(user)
   end
 
   def edit?
-    record.user == user
+    record.user.eql?(user)
   end
 
   def update?
-    record.user == user
+    record.user.eql?(user)
   end
 end
