a21f199b8319999505aa37343d10ba08647042de
sleepwalk712 <sleepwalk712@gmail.com>
2023-05-09 02:23:52 +0000

    Change Robot screen connect content
    - Change url base on task type (ex: /train/abstract_screenings/..)
    
    - Add timestamp information to prediction for finding ML model in
      ML server
    
    - Improve SQL query in getting citations
    
    - Remove duplicates in train data base on citation title
    
    - Set timeout from 10 to 300 in order to preventing disconnection

--- app/services/machine_learning_data_supplying_service.rb
+++ app/services/machine_learning_data_supplying_service.rb
@@ -1,10 +1,11 @@
 class MachineLearningDataSupplyingService
 
-  def self.get_abstract_label(project_id)
+  def self.get_labeled_abstract(project_id)
     project = Project.find(project_id)
     project_data = []
 
-    for asr in project.abstract_screening_results do
+    for asr in project.abstract_screening_results
+                      .includes(citations_project: :citation) do
       label_data = {
         'ti' => '',
         'abs' => '',
@@ -40,6 +41,45 @@ class MachineLearningDataSupplyingService
       end
 
       project_data.append(label_data)
+    end
+
+    unique_project_data = project_data.uniq { |hash| hash['ti'] }
+    return unique_project_data
+  end
+
+  def self.get_unlabel_abstract(project_id)
+    project = Project.find(project_id)
+    project_data = []
+
+    for asr in project.abstract_screening_results
+                      .includes(citations_project: :citation) do
+      label_data = {
+        'ti' => '',
+        'abs' => '',
+      }
+      citation = asr.citation
+
+      ti = citation.name.gsub('"', "'")
+      if ti.nil? || ti.empty?
+        next
+      else
+        label_data['ti'] = ti
+      end
+
+      abs = citation.abstract.gsub('"', "'")
+      if abs.nil? || abs.empty?
+        next
+      else
+        label_data['abs'] = abs
+      end
+
+      label = asr.label
+      if label.nil?
+        project_data.append(label_data)
+      else
+        next
+      end
+
     end
 
     return project_data
--- app/services/robot_screen_service.rb
+++ app/services/robot_screen_service.rb
@@ -1,19 +1,37 @@
 class RobotScreenService
 
-  def initialize(project_id, url)
+  def initialize(project_id, url, method='abstract_screenings')
     @project_id = project_id
     @url = url
+    @method = method
   end
 
   def train
-    data = MachineLearningDataSupplyingService.get_abstract_label(@project_id).first(2)
-    train_url = @url + '/train/' + @project_id.to_s
+    data = MachineLearningDataSupplyingService.get_labeled_abstract(@project_id)
+    train_url = @url + '/train/' + @method + '/' + @project_id.to_s
 
     res = HTTParty.post(
       train_url,
       {
         body: { labeled_data: data }.to_json,
-        headers: { 'Content-Type' => 'application/json', 'Accept':'application/json' }
+        headers: { 'Content-Type' => 'application/json', 'Accept':'application/json' },
+        timeout: 300
+      }
+    )
+
+    return res.body
+  end
+
+  def predict(timestamp)
+    data = MachineLearningDataSupplyingService.get_unlabel_abstract(@project_id)
+    predict_url = @url + '/predict/' + @method + '/' + @project_id.to_s
+
+    res = HTTParty.post(
+      predict_url,
+      {
+        body: { input_citations: data, timestamp: timestamp }.to_json,
+        headers: { 'Content-Type' => 'application/json', 'Accept':'application/json' },
+        timeout: 300
       }
     )
 
