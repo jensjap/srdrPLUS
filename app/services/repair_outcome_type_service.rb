85c44a3119cb51097115c4d36e5f1aaba51c0112
Jens Jap <jens@mycenaean.org>
2023-05-16 20:02:05 +0800

    Feature: Missing Type1Type
    - Adds a service that helps repair any missing type1_type
      for ExtractionsExtractionFormsProjectsSectionsType1

---
+++ app/services/repair_outcome_type_service.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# Repairs extracted outcomes without outcome type.
+#
+# Possible options for type1_type for Outcomes should be:
+# ['Continuous' ,'Categorical']
+class RepairOutcomeTypeService
+  def self.repair!(project_id = nil, get_count_only: true)
+    # Project ID 2847 'VA Airborne Hazards Constrictive Bronchiolitis and Interstitial Lung Diseases'
+    project_id = 2847 if project_id
+
+    candidates = find_candidates_for_repair(project_id)
+    puts "#{candidates.count} need repair"
+    unless get_count_only
+      candidates.each do |candidate|
+        repair_outcome_type(candidate)
+      end
+    end
+
+    puts 'done.'
+  end
+
+  # Attempt to repair based on other outcomes.
+  def self.repair_outcome_type(eefpst1)
+    template = find_template_for_repair(eefpst1)
+    if template.present?
+      eefpst1.update(type1_type: template.type1_type)
+    else
+      eefpst1.update(type1_type: Type1Type.find_by(name: 'Categorical'))
+    end
+  end
+
+  # Look for other eefpst1 and use as template.
+  # Conditions:
+  # 1. (optional) Be in the same project
+  # 1. Must have same type1.name and type1.description
+  # 1. Must have a type1_type
+  # Match the type1_type of the first one found
+  def self.find_template_for_repair(eefpst1)
+    potential_templates = query_for_template_candidates(eefpst1, local_only: true)
+    potential_templates = query_for_template_candidates(eefpst1, local_only: false) if potential_templates.blank?
+
+    potential_templates&.first
+  end
+
+  def self.query_for_template_candidates(eefpst1, local_only: true)
+    if local_only
+      ExtractionsExtractionFormsProjectsSectionsType1
+        .joins(:type1, extractions_extraction_forms_projects_section: [{ extraction: :project }])
+        .where('projects.id=?', eefpst1.extraction.project.id)
+        .where('type1s.name=? AND type1s.description=?', eefpst1.type1.name, eefpst1.type1.description)
+        .where.not(type1_type: nil)
+    else
+      ExtractionsExtractionFormsProjectsSectionsType1
+        .joins(:type1, extractions_extraction_forms_projects_section: [{ extraction: :project }])
+        .where('type1s.name=? AND type1s.description=?', eefpst1.type1.name, eefpst1.type1.description)
+        .where.not(type1_type: nil)
+    end
+  end
+
+  def self.find_candidates_for_repair(project_id = nil)
+    candidates = ExtractionsExtractionFormsProjectsSectionsType1
+                 .joins(
+                   extractions_extraction_forms_projects_section: [
+                     { extraction_forms_projects_section: :section },
+                     { extraction: :project }
+                   ]
+                 )
+                 .where('sections.name=? AND type1_type_id IS NULL', 'Outcomes')
+    candidates = candidates.where('projects.id=?', project_id) if project_id
+    candidates
+  end
+end
