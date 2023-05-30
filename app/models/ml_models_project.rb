aff93ccb557a0ad31efa994b21e7da088e655f5f
Randy Jap <randyjap@gmail.com>
2023-05-30 12:13:21 +0800

    update model annotations

--- app/models/ml_models_project.rb
+++ app/models/ml_models_project.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: ml_models_projects
+#
+#  id          :bigint           not null, primary key
+#  ml_model_id :bigint           not null
+#  project_id  :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class MlModelsProject < ApplicationRecord
   belongs_to :ml_model
   belongs_to :project
--- app/models/ml_prediction.rb
+++ app/models/ml_prediction.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: ml_predictions
+#
+#  id                   :bigint           not null, primary key
+#  citations_project_id :integer          not null
+#  ml_model_id          :bigint           not null
+#  score                :float(24)        not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
 class MlPrediction < ApplicationRecord
   belongs_to :citations_project
   belongs_to :ml_model
