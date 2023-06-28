92cfd60720b5d7cec29aac12447756ac8fce249d
Jens Jap <jens@mycenaean.org>
2023-06-28 23:43:55 +0800

    Screening Dashboard visibility
    - Set SRDRPLUS_SCREENING_DASHBOARD_USERS environmental variable
      in .env with user id to see dashboard

--- app/services/abstract_screening_service.rb
+++ app/services/abstract_screening_service.rb
@@ -2,7 +2,15 @@ class AbstractScreeningService
   def self.as_user?(user)
     return false if user.nil?
 
-    (ENV['SRDRPLUS_AS_USERS'].nil? ? [] : JSON.parse(ENV['SRDRPLUS_AS_USERS'])).include?(user.id)
+    (ENV['SRDRPLUS_AS_USERS'].nil? ? [] : JSON.parse(ENV['SRDRPLUS_AS_USERS']))
+      .include?(user.id)
+  end
+
+  def self.screening_dashboard_user?(user)
+    return false if user.nil?
+
+    (ENV['SRDRPLUS_SCREENING_DASHBOARD_USERS'].nil? ? [] : JSON.parse(ENV['SRDRPLUS_SCREENING_DASHBOARD_USERS']))
+      .include?(user.id)
   end
 
   def self.find_asr_id_to_be_resolved(abstract_screening, user, create_record = true)
