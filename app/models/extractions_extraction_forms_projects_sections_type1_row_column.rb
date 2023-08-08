a5f4519ee6e80b9b7c7beb95554e2dc2f99d0141
Randy Jap <randyjap@gmail.com>
2023-08-08 15:05:25 +0800

    update model annotations

--- app/models/extractions_extraction_forms_projects_sections_type1_row_column.rb
+++ app/models/extractions_extraction_forms_projects_sections_type1_row_column.rb
@@ -7,6 +7,7 @@
 #  timepoint_name_id                                           :integer
 #  created_at                                                  :datetime         not null
 #  updated_at                                                  :datetime         not null
+#  pos                                                         :integer          default(999999)
 #
 
 # Temporarily calling it ExtractionsExtractionFormsProjectsSectionsType1RowColumn. This is meant to be Outcome Timepoint.
