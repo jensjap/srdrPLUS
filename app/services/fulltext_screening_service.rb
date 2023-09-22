891384f8a83b527e90791aafbcb4c1db44621478
sleepwalk712 <sleepwalk712@gmail.com>
2023-09-22 06:14:29 +0000

    Bugfix: Fulltext screening crash

--- app/services/fulltext_screening_service.rb
+++ app/services/fulltext_screening_service.rb
@@ -34,9 +34,10 @@ class FulltextScreeningService < BaseScreeningService
 
   def self.get_next_singles_citation_id(fulltext_screening)
     project_screened_citation_ids = project_screened_citation_ids(fulltext_screening.project)
+    puts project_screened_citation_ids
     CitationsProject
       .joins(:screening_qualifications)
-      .where(screening_qualifications: { qualification_type: ScreeningQualification::FS_ACCEPTED })
+      .where(screening_qualifications: { qualification_type: ScreeningQualification::AS_ACCEPTED })
       .where(project: fulltext_screening.project)
       .where.not(citation_id: project_screened_citation_ids)
       .sample&.citation_id
