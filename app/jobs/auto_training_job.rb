ab6215bba0b93b4e048c1c1c4db7d45628457162
sleepwalk712 <sleepwalk712@gmail.com>
2023-10-24 08:20:29 +0000

    Add auto-predict feature
    - Fix error relation in ml_model
    
    - Add auto-predict in auto-train job
    
    - Adjust error rescue logic. Now when a project training fails
      it will start another project training instead of stop all

--- app/jobs/auto_training_job.rb
+++ app/jobs/auto_training_job.rb
@@ -2,6 +2,6 @@ class AutoTrainingJob
   include Sidekiq::Worker
 
   def perform
-    AutoTrainingService.check_and_train(100)
+    AutoTrainingService.check_and_train_and_predict(100)
   end
 end
--- app/services/auto_training_service.rb
+++ app/services/auto_training_service.rb
@@ -2,22 +2,33 @@ class AutoTrainingService
   MAX_RETRIES = 3
   URL = ENV['ML_SERVER']
 
-  def self.check_and_train(x)
-    retries = 0
-    begin
-      Project.where(auto_train: true).each do |project|
+  def self.check_and_train_and_predict(x)
+    Project.where(auto_train: true).each do |project|
+      retries = 0
+
+      begin
         new_labels_count = MachineLearningDataSupplyingService.get_labeled_abstract_since_last_train(project.id).size
         last_train_time = project.ml_models_projects.order(created_at: :desc).first&.created_at
         days_since_last_train = (Time.zone.now - (last_train_time || Time.zone.at(0))) / 1.day
 
         if new_labels_count >= x || (new_labels_count > 0 && days_since_last_train >= 7)
-          RobotScreenService.new(project.id, URL).partial_train_and_predict(80)
+          robot_service = RobotScreenService.new(project.id, URL)
+          train_result = robot_service.partial_train_and_predict(80)
+
+          if train_result.include? "Training complete"
+            timestamp = train_result.match(/model timestamp: (\S+)\)/)[1]
+            robot_service.predict(timestamp)
+          end
+        end
+
+      rescue => e
+        retries += 1
+        if retries < MAX_RETRIES
+          retry
+        else
+          Rails.logger.error "training failed for project #{project.id}: #{e.message}"
         end
       end
-    rescue => e
-      retries += 1
-      retry if retries < MAX_RETRIES
-      Rails.logger.error "training failed: #{e.message}"
     end
   end
 end
