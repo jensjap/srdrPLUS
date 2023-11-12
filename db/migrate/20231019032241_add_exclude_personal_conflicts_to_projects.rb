fd6c4c6bed2338a52bf55a761fce1d319808c0fc
Jens Jap <jens@mycenaean.org>
2023-11-12 16:34:12 +0800

    Exclude personal conflict default: false

--- db/migrate/20231019032241_add_exclude_personal_conflicts_to_projects.rb
+++ db/migrate/20231019032241_add_exclude_personal_conflicts_to_projects.rb
@@ -1,5 +1,5 @@
 class AddExcludePersonalConflictsToProjects < ActiveRecord::Migration[7.0]
   def change
-    add_column :projects, :exclude_personal_conflicts, :boolean, default: true, null: false
+    add_column :projects, :exclude_personal_conflicts, :boolean, default: false, null: false
   end
 end
