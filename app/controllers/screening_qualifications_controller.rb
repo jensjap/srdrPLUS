359f72e64bbfbd371eb90d5d2b2c29026a217586
Randy Jap <randyjap@users.noreply.github.com>
2023-12-04 14:07:58 +0800

    Correctly revert screening phases (#485)
    * reevaluate screening status when extractions are destroyed
    
    * reevaluate screening status when extractions are destroyed
    
    * return unprocessable if qualification_type is invalid
    
    * correctly revert cp screening status when deleting xsrs
    
    * reevaluate screening status when extractions are destroyed
    
    * return unprocessable if qualification_type is invalid
    
    * correctly revert cp screening status when deleting xsrs

--- app/controllers/screening_qualifications_controller.rb
+++ app/controllers/screening_qualifications_controller.rb
@@ -3,22 +3,25 @@ class ScreeningQualificationsController < ApplicationController
     respond_to do |format|
       format.json do
         qualification_type = params[:qualification_type]
+        unless ScreeningQualification::ALL_QUALIFICATIONS.include?(qualification_type)
+          return render json: [],
+                        status: :unprocessable_entity
+        end
+
         citations_projects = CitationsProject.includes(:project).where(id: params[:citations_project_ids])
         citations_projects.each { |cp| authorize(cp.project, policy_class: ScreeningQualificationPolicy) }
         results = []
         citations_projects.each do |citations_project|
-          if ScreeningQualification::ALL_QUALIFICATIONS.include?(qualification_type)
-            existing_sqs = citations_project.screening_qualifications.where(qualification_type:)
-            if existing_sqs.present?
-              existing_sqs.destroy_all
-              citations_project.abstract_screening_results.each(&:evaluate_screening_qualifications)
-              citations_project.fulltext_screening_results.each(&:evaluate_screening_qualifications)
-            else
-              if (opposite_qualification = ScreeningQualification.opposite_qualification(qualification_type))
-                citations_project.screening_qualifications.where(qualification_type: opposite_qualification).destroy_all
-              end
-              citations_project.screening_qualifications.find_or_create_by!(qualification_type:, user: current_user)
+          existing_sqs = citations_project.screening_qualifications.where(qualification_type:)
+          if existing_sqs.present?
+            existing_sqs.destroy_all
+            citations_project.abstract_screening_results.each(&:evaluate_screening_qualifications)
+            citations_project.fulltext_screening_results.each(&:evaluate_screening_qualifications)
+          else
+            if (opposite_qualification = ScreeningQualification.opposite_qualification(qualification_type))
+              citations_project.screening_qualifications.where(qualification_type: opposite_qualification).destroy_all
             end
+            citations_project.screening_qualifications.find_or_create_by!(qualification_type:, user: current_user)
           end
 
           citations_project.evaluate_screening_status
