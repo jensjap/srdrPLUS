1011fab4d78b391f76a9f971452ed860debbcd43
Jens Jap <jens@mycenaean.org>
2024-01-05 13:59:43 +0800

    Change defaults for as/fs rejection reasons

--- db/migrate/20231219063222_add_as_limit_one_reason.rb
+++ db/migrate/20231219063222_add_as_limit_one_reason.rb
@@ -1,5 +1,5 @@
 class AddAsLimitOneReason < ActiveRecord::Migration[7.0]
   def change
-    add_column :projects, :as_limit_one_reason, :boolean, default: false, null: false
+    add_column :projects, :as_limit_one_reason, :boolean, default: true, null: false
   end
 end
--- db/migrate/20231219063300_add_fs_limit_one_reason.rb
+++ db/migrate/20231219063300_add_fs_limit_one_reason.rb
@@ -1,5 +1,5 @@
 class AddFsLimitOneReason < ActiveRecord::Migration[7.0]
   def change
-    add_column :projects, :fs_limit_one_reason, :boolean, default: false, null: false
+    add_column :projects, :fs_limit_one_reason, :boolean, default: true, null: false
   end
 end
