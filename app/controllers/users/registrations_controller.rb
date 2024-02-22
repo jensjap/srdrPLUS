d25a4fe43f0ec0d9b81269c9d58b1f3f29cc0e65
Randy Jap <randyjap@users.noreply.github.com>
2024-02-22 20:02:15 +0800

    add first name, middle name, last name to sign up page (#525)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
     @nav_buttons.push('signup')
-    super
+    build_resource({})
+    resource.build_profile
+    respond_with resource
   end
 
   # POST /resource
@@ -50,9 +52,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: %i[
+                                        first_name
+                                        middle_name
+                                        last_name
+                                      ]])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
