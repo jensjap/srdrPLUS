6e83dd4d543955920a556ba598bec9012f0fbe0f
Randy Jap <randyjap@gmail.com>
2024-04-12 20:22:21 +0800

    update question form builder

--- app/models/question_row_column.rb
+++ app/models/question_row_column.rb
@@ -42,6 +42,34 @@ class QuestionRowColumn < ApplicationRecord
   delegate :question_type, to: :question_row
   delegate :project, to: :question
 
+  def form_object
+    {
+      id:,
+      cell_type: question_row_column_type.name,
+      answer_choice:
+      question_row_columns_question_row_column_options
+        .select { |qrcqrco| qrcqrco.question_row_column_option_id == 1 }.map do |qrcqrco|
+        { id: qrcqrco.id,
+          name: qrcqrco.name,
+          followup_field: qrcqrco.followup_field.then do |ff|
+            { id: ff&.id, value: ff.present? }
+          end }
+      end,
+      min_length: qrcqrcos_where_qrco(2).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } },
+      max_length: qrcqrcos_where_qrco(3).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } },
+      additional_char: qrcqrcos_where_qrco(4).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } },
+      min_value: qrcqrcos_where_qrco(5).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } },
+      max_value: qrcqrcos_where_qrco(6).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } },
+      coefficient: qrcqrcos_where_qrco(7).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } },
+      exponent: qrcqrcos_where_qrco(8).then { |qrcqrco| { id: qrcqrco.id, name: qrcqrco.name } }
+    }
+  end
+
+  def qrcqrcos_where_qrco(question_row_column_option_id)
+    question_row_columns_question_row_column_options
+      .find { |qrcqrco| qrcqrco.question_row_column_option_id == question_row_column_option_id }
+  end
+
   def field_validation_value_for(name)
     QuestionRowColumnsQuestionRowColumnOption
       .find_by!(
@@ -56,6 +84,15 @@ class QuestionRowColumn < ApplicationRecord
         eefpsqrcf.records.any? do |record|
           record.name.present?
         end
+      end
+    end
+  end
+
+  def remove_column_from_all_siblings
+    removal_index = question_row.question_row_columns.find_index(self)
+    question_row.question.question_rows.each do |question_row|
+      question_row.question_row_columns.each_with_index do |question_row_column, question_row_column_index|
+        question_row_column.destroy if question_row_column_index == removal_index
       end
     end
   end
@@ -76,8 +113,8 @@ class QuestionRowColumn < ApplicationRecord
   end
 
   def ensure_question_row_column_fields
-    if question_row_column_type.name == QuestionRowColumnType::NUMERIC  # Numeric requires 2 fields.
-      question_row_column_fields.create! while question_row_column_fields.length < 2
-    end
+    return unless question_row_column_type.name == QuestionRowColumnType::NUMERIC  # Numeric requires 2 fields.
+
+    question_row_column_fields.create! while question_row_column_fields.length < 2
   end
 end
--- app/policies/question_row_columns_question_row_column_option_policy.rb
+++ app/policies/question_row_columns_question_row_column_option_policy.rb
@@ -2,4 +2,12 @@ class QuestionRowColumnsQuestionRowColumnOptionPolicy < ApplicationPolicy
   def destroy?
     project_consolidator?
   end
+
+  def update?
+    project_consolidator?
+  end
+
+  def create?
+    project_consolidator?
+  end
 end
