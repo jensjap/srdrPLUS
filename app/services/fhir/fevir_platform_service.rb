a93e0b78b38031260d1c8405bd799092b2cfa996
sleepwalk712 <sleepwalk712@yahoo.com.tw>
2024-05-01 01:54:56 +0000

    Create service for posting fhir to fevir

---
+++ app/services/fhir/fevir_platform_service.rb
@@ -0,0 +1,25 @@
+class FevirPlatformService
+  API_ENDPOINT = 'https://api.fevir.net'
+
+  def initialize(project_id, api_token)
+    @project_id = project_id
+    @api_token = api_token
+  end
+
+  def submit_resource
+    fhir_entry = AllResourceSupplyingService.new.find_by_project_id(@project_id)
+    post_data = {
+      functionid: 'submitfhirresource',
+      tool: 'directfhirentry',
+      fhirEntry: fhir_entry,
+      status: 'active',
+      resourceType: 'Bundle',
+      apiToken: @api_token
+    }
+
+    response = HTTParty.post(API_ENDPOINT,
+                             body: post_data.to_json,
+                             headers: { 'Content-Type' => 'application/json' })
+    response.parsed_response
+  end
+end
