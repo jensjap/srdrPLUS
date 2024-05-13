7b80edda861d389f12bd24d870564ca34277cd90
sleepwalk712 <sleepwalk712@yahoo.com.tw>
2024-05-13 05:47:55 +0000

    Bugfix: List identifier should be array not dict

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_14_094624) do
-  create_table "abstrackr_settings", id: :integer, charset: "utf8mb3", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2024_05_08_055656) do
+  create_table "abstrackr_settings", id: :integer, charset: "utf8", force: :cascade do |t|
     t.integer "profile_id"
     t.boolean "authors_visible", default: true
     t.boolean "journal_visible", default: true
