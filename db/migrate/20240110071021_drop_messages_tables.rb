4aaaf2d00824f8135b94eb2284902bfe33f00a57
Randy Jap <randyjap@gmail.com>
2024-06-24 15:23:53 +0800

    remove dropping tables that don't exist

--- db/migrate/20240110071021_drop_messages_tables.rb
+++ db/migrate/20240110071021_drop_messages_tables.rb
@@ -1,8 +1,8 @@
 class DropMessagesTables < ActiveRecord::Migration[7.0]
   def change
-    drop_table :message_boards
-    drop_table :mb_messages
-    drop_table :mb_reads
+    # drop_table :message_boards
+    # drop_table :mb_messages
+    # drop_table :mb_reads
 
     drop_table :messages
     drop_table :message_types
