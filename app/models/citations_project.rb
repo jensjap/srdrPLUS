73b37b31206499d01e3c1675e2cb533e87ddabde
Jens Jap <jens@mycenaean.org>
2024-07-14 02:11:56 -0400

    Limit author map string to 20000 characters.

--- app/models/citations_project.rb
+++ app/models/citations_project.rb
@@ -157,7 +157,7 @@ class CitationsProject < ApplicationRecord
       'citations_project_id' => id,
       'citation_id' => citation.id,
       'accession_number_alts' => citation.accession_number_alts,
-      'author_map_string' => citation.authors.to_s,
+      'author_map_string' => citation.authors.to_s.truncate(20_000),
       'name' => citation.name.to_s,
       'year' => citation.year.to_s,
       'publication' => "#{citation.journal&.name} #{citation.journal&.volume}(#{citation.journal&.issue}):#{citation&.page_number_start}-#{citation&.page_number_end}",
