aeb93abe67ec02d3e72858965f55e0c553f06fc8
Jens Jap <jens@mycenaean.org>
2024-07-29 16:16:52 +0000

    Chore: Whitelist domain names.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+  config.hosts = [
+    "vadrr.org",
+    "www.vadrr.org"   # Allow this to be addressed when running in containers via docker-compose.yml.
+  ]
   # config.after_initialize do
   #   Bullet.enable = true
   #   Bullet.bullet_logger = true
