e806dd739f8603e065f6ab13ccc4cb2eff72c458
Jens Jap <jens@mycenaean.org>
2024-08-18 11:33:55 -0700

    Feature: Amoeba
    - Move amoeba copy responsibility of KeyQuestionsProjectsQuestion to its own model

--- app/models/key_questions_projects_question.rb
+++ app/models/key_questions_projects_question.rb
@@ -10,6 +10,28 @@
 #
 
 class KeyQuestionsProjectsQuestion < ApplicationRecord
+  attr_accessor :is_amoeba_copy
+
+  amoeba do
+    customize(lambda { |_, copy|
+      copy.is_amoeba_copy = true
+    })
+  end
+
+  before_commit :correct_parent_associations, if: :is_amoeba_copy
+
   belongs_to :key_questions_project, inverse_of: :key_questions_projects_questions
   belongs_to :question,              inverse_of: :key_questions_projects_questions
+
+  private
+
+  def correct_parent_associations
+    return unless is_amoeba_copy
+
+    correct_key_questions_project_association
+  end
+
+  def correct_key_questions_project_association
+    key_questions_project.update(project: question.project)
+  end
 end
