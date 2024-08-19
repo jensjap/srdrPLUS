8731f753cd512f7d9762f4c6a1e0b1b625196b10
Jens Jap <jens@mycenaean.org>
2024-08-18 23:55:48 -0700

    Chore: Renaming methods and clean up correcting KeyQuestionsProject association on ExtractionsKeyQuestionsProjectsSelection

--- app/models/extractions_key_questions_projects_selection.rb
+++ app/models/extractions_key_questions_projects_selection.rb
@@ -11,24 +11,26 @@
 class ExtractionsKeyQuestionsProjectsSelection < ApplicationRecord
   attr_accessor :is_amoeba_copy
 
-  before_commit :correct_kqs_association
-
   amoeba do
     customize(lambda { |_, copy|
       copy.is_amoeba_copy = true
     })
   end
 
+  before_commit :correct_parent_associations
+
   belongs_to :extraction
   belongs_to :key_questions_project
 
   private
 
-  def correct_kqs_association
+  def correct_parent_associations
     return unless is_amoeba_copy
 
-    key_question_id = key_questions_project.key_question_id
-    key_questions_project_id = extraction.project.key_questions_projects.find_by(key_question_id:).id
-    update(key_questions_project_id:)
+    correct_kqs_association
+  end
+
+  def correct_kqs_association
+    update(key_questions_project: extraction.project.key_questions_projects.find_by(key_question: key_questions_project.key_question))
   end
 end
