e4e4626f6de2722bc9cf2220fd2fb5001c0a0e25
Jens Jap <jens@mycenaean.org>
2024-09-03 05:43:38 -0700

    Amoeba: Comparate boilerplate

--- app/models/comparate.rb
+++ app/models/comparate.rb
@@ -10,7 +10,17 @@
 #
 
 class Comparate < ApplicationRecord
+  attr_accessor :is_amoeba_copy
+
+  amoeba do
+    customize(lambda { |_, copy|
+      copy.is_amoeba_copy = true
+    })
+  end
+
   after_commit :set_extraction_stale, on: %i[create update destroy]
+
+  before_commit :correct_parent_associations, if: :is_amoeba_copy
 
   belongs_to :comparate_group,    inverse_of: :comparates
   belongs_to :comparable_element, inverse_of: :comparates
@@ -25,5 +35,11 @@ class Comparate < ApplicationRecord
         crss.result_statistic_section.population.extraction.extraction_checksum.update(is_stale: true)
       end
     end
+  end
+
+  def correct_parent_associations
+    return unless is_amoeba_copy
+
+    # Placeholder for debugging. No corrections needed.
   end
 end
