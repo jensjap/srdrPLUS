d8e6d45a43891f825b8ef5337e1489ee81bf2bd9
Randy Jap <randyjap@gmail.com>
2024-09-14 11:15:35 +0800

    able to send project messages in dashboard

--- app/channels/help_channel.rb
+++ app/channels/help_channel.rb
@@ -2,14 +2,14 @@ class HelpChannel < ApplicationCable::Channel
   def subscribed
     # TODO: authorize
     message_keys = params[:messageKeys]
-    project = Project.find(message_keys[:project])
-    citations_project = CitationsProject.find(message_keys[:citations_project])
+    project = Project.find_by(id: message_keys[:project])
+    citations_project = CitationsProject.find_by(id: message_keys[:citations_project])
     efps = ExtractionFormsProjectsSection.find_by(id: message_keys[:efps])
-    extraction = Extraction.find(message_keys[:extraction])
+    extraction = Extraction.find_by(id: message_keys[:extraction])
 
-    stream_from "project-#{project.id}"
-    stream_from "citations_project-#{citations_project.id}-efps-#{efps&.id}"
-    stream_from "extraction-#{extraction.id}"
+    stream_from "project-#{project.id}" if project
+    stream_from "citations_project-#{citations_project.id}-efps-#{efps&.id}" if citations_project
+    stream_from "extraction-#{extraction.id}" if extraction
   end
 
   def unsubscribed
