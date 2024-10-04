a312dc31fbde7923867709531b39d2095b4d1c84
Randy Jap <randyjap@gmail.com>
2024-10-04 19:02:03 +0800

    fix policy name

--- app/channels/help_channel.rb
+++ app/channels/help_channel.rb
@@ -5,13 +5,13 @@ class HelpChannel < ApplicationCable::Channel
     citations_project = CitationsProject.find_by(id: message_keys[:citations_project])
     extraction = Extraction.find_by(id: message_keys[:extraction])
 
-    stream_from "project-#{project.id}" if project && MessagePolicy.new(current_user,
-                                                                        citations_project.project).subscribed?
-    stream_from "citations_project-#{citations_project.id}" if citations_project && MessagePolicy.new(
+    stream_from "project-#{project.id}" if project && HelpChannelPolicy.new(current_user,
+                                                                            citations_project.project).subscribed?
+    stream_from "citations_project-#{citations_project.id}" if citations_project && HelpChannelPolicy.new(
       current_user, citations_project.project
     ).subscribed?
-    stream_from "extraction-#{extraction.id}" if extraction && MessagePolicy.new(current_user,
-                                                                                 extraction.project).subscribed?
+    stream_from "extraction-#{extraction.id}" if extraction && HelpChannelPolicy.new(current_user,
+                                                                                     extraction.project).subscribed?
   end
 
   def unsubscribed
--- app/policies/help_channel_policy.rb
+++ app/policies/help_channel_policy.rb
@@ -1,4 +1,4 @@
-class MessagePolicy < ApplicationPolicy
+class HelpChannelPolicy < ApplicationPolicy
   def subscribed?
     part_of_project?
   end
