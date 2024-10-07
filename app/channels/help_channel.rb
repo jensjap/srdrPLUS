693b6f9380289ecb7ec3b8a1c62d9fe2862e113a
Randy Jap <randyjap@gmail.com>
2024-10-07 14:46:34 +0800

    fix typo

--- app/channels/help_channel.rb
+++ app/channels/help_channel.rb
@@ -6,7 +6,7 @@ class HelpChannel < ApplicationCable::Channel
     extraction = Extraction.find_by(id: message_keys[:extraction])
 
     stream_from "project-#{project.id}" if project && HelpChannelPolicy.new(current_user,
-                                                                            citations_project.project).subscribed?
+                                                                            project).subscribed?
     stream_from "citations_project-#{citations_project.id}" if citations_project && HelpChannelPolicy.new(
       current_user, citations_project.project
     ).subscribed?
