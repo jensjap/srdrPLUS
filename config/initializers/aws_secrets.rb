c926de2f163e14205e512d09fb8ba6ae281b2657
Jens Jap <jens@vadrr.org>
2025-02-24 14:36:48 +0000

    Implement AWS Secrets Manager
    - Use AWS Secret Manager to handle database credentials
    - Move sensitive information from docker.env into private.env

---
+++ config/initializers/aws_secrets.rb
@@ -0,0 +1,19 @@
+require 'aws-sdk-secretsmanager'
+
+Aws.config.update({
+  region: ENV['AWS_REGION'],
+  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+})
+
+# Create a Secrets Manager client
+secrets_manager_client = Aws::SecretsManager::Client.new
+
+# Example: Fetch a secret
+secret_name = ENV['AWS_SECRET_NAME']
+begin
+  response = secrets_manager_client.get_secret_value(secret_id: secret_name)
+  secret = response.secret_string
+  DATABASE_CREDENTIALS = JSON.parse(secret)
+rescue Aws::SecretsManager::Errors::ServiceError => e
+  puts "Error fetching secret: #{e.message}"
+end
