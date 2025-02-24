456dcc3571c2a94ee220e13440f1e071d0603cbb
Jens Jap <jens@vadrr.org>
2025-02-24 14:49:46 +0000

    Modify docker compose to run on production

--- entrypoints/docker-entrypoint.sh
+++ entrypoints/docker-entrypoint.sh
@@ -6,4 +6,6 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+export RAILS_ENV=production
+export RAILS_SERVE_STATIC_FILES=true
 bundle exec rails s -b 0.0.0.0
--- entrypoints/sidekiq-entrypoint.sh
+++ entrypoints/sidekiq-entrypoint.sh
@@ -6,4 +6,5 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+export RAILS_ENV=production
 bundle exec sidekiq -C config/sidekiq.yml
