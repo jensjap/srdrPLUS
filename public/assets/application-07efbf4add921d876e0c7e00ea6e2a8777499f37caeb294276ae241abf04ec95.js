afa5d5d4cd74ff5288ee096efc59c2427c810b23
Jens Jap <jens@mycenaean.org>
2025-10-18 15:49:17 -0400

    Update assets.

--- public/assets/application-07efbf4add921d876e0c7e00ea6e2a8777499f37caeb294276ae241abf04ec95.js
+++ public/assets/application-07efbf4add921d876e0c7e00ea6e2a8777499f37caeb294276ae241abf04ec95.js
@@ -65996,17 +65996,6 @@ function __guardMethod__(obj, methodName, transform) {
     return undefined;
   }
 };
-;(function (g) {
-  var d = document, am = d.createElement('script'), h = d.head || d.getElementsByTagName("head")[0], fsr = 'fsReady',
-  aex = {
-    "src": "//gateway.foresee.com/sites/ahrq/production/gateway.min.js",
-    "type": "text/javascript",
-    "async": "true",
-    "data-vendor": "fs",
-    "data-role": "gateway"
-  };
-  for (var attr in aex) { am.setAttribute(attr, aex[attr]); } h.appendChild(am); g[fsr] || (g[fsr] = function () { var aT = '__' + fsr + '_stk__'; g[aT] = g[aT] || []; g[aT].push(arguments); });
-})(window);
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
     return (function() {
@@ -69062,7 +69051,6 @@ function __guardMethod__(obj, methodName, transform) {
   });
 
 }).call(this);
-
 
 
 
